e3f36a9d4a407e4d2766413d5542339d5a53199e
park4084 <akscjfals@naver.com>
2022-08-17 00:10:35 +0900

    Question : 3003 chess number

--- Challenge.c
+++ Challenge.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 int main () {
-    char name[50];
+    int king, queen, rook, bishop, knight, pawn  = 0;
+    
+    scanf("%d %d %d %d %d %d", &king, &queen, &rook, &bishop, &knight, &pawn);
 
-    scanf("%s", name);
-
-    printf("%s", name);
-    printf("?");
-    printf("?");
-    printf("!");
+    printf("%d %d %d %d %d %d", 1 - (king), 1 - (queen), 2 - (rook), 2 - (bishop), 2 - (knight), 8 - (pawn));
     
     return 0;
 }
