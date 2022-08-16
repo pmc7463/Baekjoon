80fc91ac5be2c16208946e182b34e6aee54ab5fa
park4084 <akscjfals@naver.com>
2022-08-17 00:28:56 +0900

    dog

--- Challenge.c
+++ Challenge.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main () {
-    int king, queen, rook, bishop, knight, pawn  = 0;
-    
-    scanf("%d %d %d %d %d %d", &king, &queen, &rook, &bishop, &knight, &pawn);
-
-    printf("%d %d %d %d %d %d", 1 - (king), 1 - (queen), 2 - (rook), 2 - (bishop), 2 - (knight), 8 - (pawn));
-    
-    return 0;
+    printf("|\\_/|");
+    printf("\n");
+    printf("|q p|   /}");
+    printf("\n");
+    printf("( 0 )\"\"\"\\");
+    printf("\n");
+    printf("|\"^\"`    |");
+    printf("\n");
+    printf("||_/=\\\\__|");
+   return 0;
 }
