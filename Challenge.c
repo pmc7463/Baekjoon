96e3a294d3bcb147cc6bc2cf733c80c1fcf3a374
park4084 <akscjfals@naver.com>
2022-08-18 23:03:00 +0900

    problem 2753 leap year

--- Challenge.c
+++ Challenge.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 
 int main () {
-    int a[3] = {0, };
-    int b[3] = {0, };;
-    int i = 0;
-    int add[3] = {0, };
+    int year = 0;
 
-    for(i = 0; i < 3; i++) {
-        scanf("%1d", &a[i]);
-    }
-
-    for(i = 0; i < 3; i++) {
-        scanf("%1d", &b[i]);
-    }
-
-    //scanf("%d %d %d", &b[0], &b[1], &b[2]);
-
-    //printf("%d", a[0] * 100 + a[1] * 10 + a[2]);
-    for (i = 2; i > -1; i--){
-        add[i] = (a[0] * 100 * b[i]) + (a[1] * 10 * b[i]) + (a[2] * b[i]);
-        printf("%d\n", add[i]);
-    }
+    scanf("%d", &year);
     
-    printf("%d", add[0] * 100 + add[1] * 10 + add[2]);
+    if(year % 4 == 0){              // 1단계
+        if(year % 100 == 0) {       // 2단계
+            if(year % 400 == 0){    // 3단계
+                printf("1");        // 최종 윤년
+            }
+            else
+                printf("0");        // 3단계 윤년 아님
+        }
+        else
+            printf("1");            // 2단계 윤년
+    }
+    else 
+        printf("0");                // 1단계 윤년 아님
+        
    return 0;
 }
