5ab94380520797e9c356a0f05248a842f77135b2
park4084 <akscjfals@naver.com>
2022-08-19 23:55:49 +0900

    problem 2525

--- Challenge.c
+++ Challenge.c
@@ -2,18 +2,38 @@
 
 int main () {
     int H, M, clock = 0;
-    
-    scanf("%d %d", &H, &M);
+    int x, y, z= 0;
 
-    if((0 <= H && H <= 23) && (0 <= M && M <= 59)){    
-        if(M >= 45){
-            printf("%d %d", H, M - 45);
-        }
-        else {  // M < 45
-            if(H - 1 < 0)
-                printf("%d %d", H + 23, M + 15);
-            else
-                printf("%d %d", H - 1, M + 15);
+    scanf("%d %d", &H, &M);
+    scanf("%d", &clock);
+
+    if((0 <= H && H <= 23) && (0 <= M && M <= 59) && (0 <= clock && clock <= 1000)){    
+        if(clock + M < 60)  // 60분 넘으면 H 고려하기 위해서 조건문 사용
+            printf("%d %d", H, M + clock);
+        else {
+            x = (M + clock) / 60;   // 1시간 이상
+            y = clock % 60;         // 남은 분 량 
+
+            if (M + y >= 60) {
+                if(H + x < 23)  // 현재 시간 + 설정 시간 = 23시 미만 
+                    printf("%d %d", H + x, M + y - 60);
+                else if (H + x == 23)   // 현재 시간 + 설정 시간 == 23시 일 때
+                    printf("%d %d",H + x, M + y - 60);
+                else {          // 현재 시간 + 설정 시간 = 23시 이상
+                    if(M + clock >= 60) // 현재 시간 + 설정 시간 = 23시 이상 + 60분 이상 값
+                        printf("%d %d",H + x - 24, M + y - 60);
+                }
+            }
+            else {
+                if(H + x < 23)  // 현재 시간 + 설정 시간 = 23시 미만 
+                    printf("%d %d", H + x, M + y);
+                else if (H + x == 23)   // 현재 시간 + 설정 시간 == 23시 일 때
+                    printf("%d %d",H + x, M + y);
+                else {          // 현재 시간 + 설정 시간 = 23시 이상
+                    if(M + clock >= 60) // 현재 시간 + 설정 시간 = 23시 이상 + 60분 이상 값
+                        printf("%d %d",H + x - 24, M + y);
+                 }
+            }   
         }
     }
     else return 0;   // 입력 에러
