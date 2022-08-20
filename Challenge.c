56528c48663da8a93e1d51d0a448c18200d6b5f1
park4084 <akscjfals@naver.com>
2022-08-21 01:17:02 +0900

    2480 problem 3 dice

--- Challenge.c
+++ Challenge.c
@@ -1,42 +1,52 @@
 #include <stdio.h>
 
-int main () {
-    int H, M, clock = 0;
-    int x, y, z= 0;
+int main() {
+    int a, b, c, sum = 0;
 
-    scanf("%d %d", &H, &M);
-    scanf("%d", &clock);
+    for( ; ; ) { // 초깃값, 조건식, 변환식을 모두 생략하면 무한 루프
+        scanf("%d %d %d", &a, &b, &c);
 
-    if((0 <= H && H <= 23) && (0 <= M && M <= 59) && (0 <= clock && clock <= 1000)){    
-        if(clock + M < 60)  // 60분 넘으면 H 고려하기 위해서 조건문 사용
-            printf("%d %d", H, M + clock);
-        else {
-            x = (M + clock) / 60;   // 1시간 이상
-            y = clock % 60;         // 남은 분 량 
-
-            if (M + y >= 60) {
-                if(H + x < 23)  // 현재 시간 + 설정 시간 = 23시 미만 
-                    printf("%d %d", H + x, M + y - 60);
-                else if (H + x == 23)   // 현재 시간 + 설정 시간 == 23시 일 때
-                    printf("%d %d",H + x, M + y - 60);
-                else {          // 현재 시간 + 설정 시간 = 23시 이상
-                    if(M + clock >= 60) // 현재 시간 + 설정 시간 = 23시 이상 + 60분 이상 값
-                        printf("%d %d",H + x - 24, M + y - 60);
-                }
-            }
-            else {
-                if(H + x < 23)  // 현재 시간 + 설정 시간 = 23시 미만 
-                    printf("%d %d", H + x, M + y);
-                else if (H + x == 23)   // 현재 시간 + 설정 시간 == 23시 일 때
-                    printf("%d %d",H + x, M + y);
-                else {          // 현재 시간 + 설정 시간 = 23시 이상
-                    if(M + clock >= 60) // 현재 시간 + 설정 시간 = 23시 이상 + 60분 이상 값
-                        printf("%d %d",H + x - 24, M + y);
-                 }
-            }   
+        if( a && b && c > 0 && a && b && c < 7) {
+            break;        
         }
     }
-    else return 0;   // 입력 에러
 
-   return 0;
+    if (a == b && a == c && b == c){
+        sum = a * 1000;
+        sum = sum + 10000;
+    }
+
+    else if (a == b || a == c || b == c) {
+        if(a == b){
+            sum = a * 100;
+            sum = sum +1000;
+        }
+        else if(a == c){
+            sum = a * 100;
+            sum = sum +1000;
+        }
+        else {
+            sum = b * 100;
+            sum = sum +1000;
+        }
+    }
+    
+    else {
+        if(a > b){
+            if (a > c)
+                sum = a * 100;
+            else
+                sum = c * 100;    
+        }
+        else {  // a < b
+            if (b > c)
+                sum = b * 100;
+            else
+                sum = c * 100;
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
 }
