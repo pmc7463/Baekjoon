ef04a4fdc11f6272eeb2b5bc72472c567cfc5cae
park4084 <akscjfals@naver.com>
2022-08-21 02:03:21 +0900

    problem 2744 A <-> a

--- Challenge.c
+++ Challenge.c
@@ -1,52 +1,16 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, c, sum = 0;
-
-    for( ; ; ) { // 초깃값, 조건식, 변환식을 모두 생략하면 무한 루프
-        scanf("%d %d %d", &a, &b, &c);
-
-        if( a && b && c > 0 && a && b && c < 7) {
-            break;        
-        }
-    }
-
-    if (a == b && a == c && b == c){
-        sum = a * 1000;
-        sum = sum + 10000;
-    }
-
-    else if (a == b || a == c || b == c) {
-        if(a == b){
-            sum = a * 100;
-            sum = sum +1000;
-        }
-        else if(a == c){
-            sum = a * 100;
-            sum = sum +1000;
-        }
-        else {
-            sum = b * 100;
-            sum = sum +1000;
-        }
-    }
+    char a[100];
     
-    else {
-        if(a > b){
-            if (a > c)
-                sum = a * 100;
-            else
-                sum = c * 100;    
+    scanf("%s", &a[0]);
+    for(int i = 0; a[i] != 0; i++){
+        if(a[i] >= 'a' && a[i] <= 'z'){
+            a[i] = a[i] - 32;
         }
-        else {  // a < b
-            if (b > c)
-                sum = b * 100;
-            else
-                sum = c * 100;
-        }
+        else
+            a[i] = a[i] + 32;
     }
-
-    printf("%d", sum);
-
+    printf("%s", a);
     return 0;
 }
