ee89c084f4fd57d36b821363e595406472e57b5b
park4084 <akscjfals@naver.com>
2022-08-21 23:41:00 +0900

    poublem 10951 EOF

--- Challenge.c
+++ Challenge.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
-#include <string.h>
-
-char text[100];
 
 int main() {    
-    int i, len;
-    while(1){
-        scanf("%[^\n]", text);
-        len = strlen(text);
-        if(text[0] == '*' && text[1] == '*' && text[2] == '*' && len == 3) break;
-        for(i = len - 1; i >= 0; i--)
-            printf("%c", text[i]);
-    }
+    int a, b = 0; 
+
+    while (scanf("%d %d", &a, &b) != EOF)
+        printf("%d\n", a + b);
+    
     return 0;
 }
