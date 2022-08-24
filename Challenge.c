b2c85fff04031083b13d2b51690d2354320ed8a1
park4084 <akscjfals@naver.com>
2022-08-24 09:05:45 +0900

    problem 2562 max

--- Challenge.c
+++ Challenge.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
-
 int main() {
-    int num, compare, again = 0;
+    int arr[10], big = 0, index;
+    for (int i = 1; i <= 9; i++)
+    {
+        scanf("%d", &arr[i]);
+        if (arr[i] > big)
+        {
+            big = arr[i];
+            index = i;
+        }
+    }
+    printf("%d\n%d", big, index);
 
-    scanf("%d", &num);
-
-    compare = num;
-
-    do{
-        compare = (compare % 10) * 10 + (compare / 10 + compare % 10) % 10;
-        again++;
-    } while(compare != num);
-    
-    printf("%d", again);
-    
     return 0;
 }
