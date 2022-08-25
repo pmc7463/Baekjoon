ebb41ba12ba2fa406933beac90d23ee1b350b321
park4084 <akscjfals@naver.com>
2022-08-25 16:22:24 +0900

    problum 1546 array average

--- Challenge.c
+++ Challenge.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
 int main() {
-	int arr[10], i, j;
-	int count = 1; // 서로 다른 값의 갯수
-	
-	for(i = 0; i < 10; i++) {
-		scanf("%d", &arr[i]);
-		arr[i] %= 42;
-	}
-	
-	for(i = 0; i < 9; i++) {
-		for(j = i + 1; j < 10; j++)
-			if(arr[i] == arr[j]) {
-                break; // 서로 같은 수일 경우
-            }
-            if( j == 10 ) 
-                count++; // 서로 같은 수가 없는 경우
-	}
+	int subject, max = 0;
+    int arr[1000];
+    double sum[1000] = {0, };
+    double average = 0;
+    double temp;
 
-	printf("%d",count);
+    scanf("%d", &subject);
+
+    for(int i = 0; i < subject; i++) {
+        scanf("%d", &arr[i]);
+
+        if(arr[i] > max)
+            max = arr[i];
+    }
+
+    for(int i = 0; i < subject; i++) {
+
+        sum[i] = (arr[i] / (double)max) * 100;
+        
+        temp += sum[i];
+
+        average = temp / subject;
+    }
+
+    printf("%lf\n", average);
 
     return 0;
 }
