027ef2f3977468725ef9628e4e9e5df55e3d30ec
park4084 <akscjfals@naver.com>
2022-08-25 14:39:49 +0900

    poublem 3052 arr mod

--- Challenge.c
+++ Challenge.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
+
 int main() {
-    int arr[10], big = 0, index;
-    for (int i = 1; i <= 9; i++)
-    {
-        scanf("%d", &arr[i]);
-        if (arr[i] > big)
-        {
-            big = arr[i];
-            index = i;
-        }
-    }
-    printf("%d\n%d", big, index);
+	int arr[10], i, j;
+	int count = 1; // 서로 다른 값의 갯수
+	
+	for(i = 0; i < 10; i++) {
+		scanf("%d", &arr[i]);
+		arr[i] %= 42;
+	}
+	
+	for(i = 0; i < 9; i++) {
+		for(j = i + 1; j < 10; j++)
+			if(arr[i] == arr[j]) {
+                break; // 서로 같은 수일 경우
+            }
+            if( j == 10 ) 
+                count++; // 서로 같은 수가 없는 경우
+	}
+
+	printf("%d",count);
 
     return 0;
 }
