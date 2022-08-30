7b7104321f2ad0067dd7faf6c17f63912b032365
changstudent <akscjfals@naver.com>
2022-08-30 23:25:31 +0900

    problem 2558 A + B

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,37 +1,13 @@
 #include <stdio.h>
 
 int main() {
-	int input = 0, count = 0;
-	int alpabet[26] = {0};
-	char str[101];
+	int a = 0, b = 0;
+
+	scanf("%d", &a);
+	scanf("%d", &b);
 	
-	scanf("%d", &input);
-
-	for(int i = 0; i < input; i++) {
-		scanf("%s", str);
-		for(int j = 0; j < 101; j++){
-			if(alpabet[str[j] - 'a'] == 0 || j - alpabet[str[j] - 'a'] < 2) {
-				if(str[0] == str[j] && j - alpabet[str[j] - 'a'] > 1)
-					break;
-				alpabet[str[j]  - 'a'] = j;
-			}
-			// 알파벳이 연속되는지 확인
-			else if(j - alpabet[str[j] - 'a'] > 1)
-				break;
-			//알파벳이 연속되지 않았을 때 break
-			if(str[j] == '\0') {
-				count++;
-				break;
-			}
-			//단어에 알파벳이 연속되지 않는 부분이 없을 때 count에 1을 추가
-		}
-		// 초기화
-		for(int i = 0; i < 101; i++)
-			str[i] = '\0';
-		for(int i = 0; i < 26; i++)
-			alpabet[i] = 0;
-	}
-	printf("%d", count);
-
+	if(a > 0 && a < 10 && b > 0 && b < 10)
+		printf("%d", a + b);
+	
 	return 0;
 }
