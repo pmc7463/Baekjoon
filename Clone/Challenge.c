e3bd30c94bf8777777a7105b64e5ffa0cbbf3bb4
changstudent <akscjfals@naver.com>
2022-09-05 07:17:41 +0900

    [problem 2839] Sugar Delivery

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 
 int main() {
-	int arr[15][15] = {0,};	// 아파트의 층은 14보다
-	//작거나 같기 때문에 각각 15크기의 2중 배열을 선언하고 0으로 초기화
-	int test, layer, room;
+	int sugar, amount, bag;
+	int i = 1;
+	scanf("%d", &sugar);
 
-	for (int i = 0; i < 15; i++) {
-		arr[0][i] = i;	// 0층의 i호에는 i명 만큼 살기때문에 0층에는 i명으로 선언
+	while (1) {
+		amount = sugar - (3 * i);
+
+		if (sugar % 5 == 0) {
+			bag = sugar / 5;
+			break;
+		} 
+
+		if (amount % 5 == 0 || amount == 0) {	// 5로 나누어 떨어지거나 amount가 0이면 3의 배수이다.
+			bag = amount / 5 + i;
+			break;
+		}
+
+		if (amount <= 2) {
+			bag = -1;
+			break;
+		}
+		i++;
 	}
-
-	for (int i = 1; i < 15; i++) {
-		for (int j = 1; j < 15; j++) {
-			arr[i][j] = arr[i - 1][j] + arr[i][j - 1];
-		}	//밑에층의 1호부터 해당호까지 사람들의 합을 arr[i][j]에 넣어준다. 
-	}
-
-	scanf("%d", &test);
-
-	for (int i = 0; i < test; i++) {
-		scanf("%d %d", &layer, &room);
-		printf("%d\n", arr[layer][room]);
-	}
+	printf("%d", bag);
 	return 0;
 }
