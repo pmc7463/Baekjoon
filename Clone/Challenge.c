7f79938e8d15382d2d2b5aafbb40364dee2b5f13
changstudent <akscjfals@naver.com>
2022-09-06 19:27:07 +0900

    [problem 10757] Big number A + B

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,29 +1,52 @@
 #include <stdio.h>
+#include <string.h>
+
+void reverse(char arr[]) {	// 문자열 역순 정렬
+	int len = strlen(arr);
+	for (int i = 0; i < len / 2; i++) {
+		char temp = arr[i];
+		arr[i] = arr[len - i - 1];
+		arr[len - i - 1] = temp; 
+	}
+}
 
 int main() {
-	int sugar, amount, bag;
-	int i = 1;
-	scanf("%d", &sugar);
+	char A[10002] = {0};
+	char B[10002] = {0};
+	char res[10003] = {0};
+	int carry = 0;	// 자릿수 올림
+	int len = 0;
 
-	while (1) {
-		amount = sugar - (3 * i);
+	scanf("%s %s", A, B);
 
-		if (sugar % 5 == 0) {
-			bag = sugar / 5;
-			break;
-		} 
+	reverse(A);
+	reverse(B);
 
-		if (amount % 5 == 0 || amount == 0) {	// 5로 나누어 떨어지거나 amount가 0이면 3의 배수이다.
-			bag = amount / 5 + i;
-			break;
-		}
-
-		if (amount <= 2) {
-			bag = -1;
-			break;
-		}
-		i++;
+	if (strlen(A) > strlen(B)) {	// 큰 수 기준
+		len = strlen(A);
 	}
-	printf("%d", bag);
+	else {
+		len = strlen(B);
+	}
+
+	for (int i = 0; i < len; i++) {
+		int sum = A[i] - '0' + B[i] - '0' + carry;
+
+		if (sum < 0) {	// 자리수가 다를경우 (sum < 0) sum은 -'0' 이 한번 더 계산됨 (둘 중하나는 NULL이므로)
+			sum += '0';	// 그런 경우 + '0'을 해서 맞춰준다.
+		}
+		if (sum > 9) {
+			carry = 1;	// 더한 값이 9를 넘어가면 그 다음자리수 +1 해줘야 하므로
+		}
+		else {
+			carry = 0;
+		}
+		res[i] = sum % 10 + '0';
+	}
+	if (carry == 1) {
+		res[len] = '1';
+	}
+	reverse(res);	// 180도 돌려 원상복구
+	printf("%s", res);
 	return 0;
 }
