5fa8c5eb669337a4414e2bbe82aa1d7c95555af6
changstudent <akscjfals@naver.com>
2022-09-09 08:55:05 +0900

    [problem 2475] Number of Validations

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,30 +1,20 @@
 #include <stdio.h>
 
 int main() {
-	int number[8] = {0};
-	int count1 = 0, count2 = 0;
-	int temp1[] = {1,2,3,4,5,6,7,8};
-	int temp2[] = {8,7,6,5,4,3,2,1};
+	int number[4] = {0};
+	int sum = 0;
 
-	for (int i = 0; i < 8; i++) {
+	for (int i = 0; i < 5; i++) {
 		scanf("%d", &number[i]);
-		if (number[i] < 0 || number[i] > 8)
-			return 0;
 	}
 
-	for (int i = 0; i < 8; i++) {
-		if (temp1[i] == number[i])
-			count1++;
-		if (temp2[i] == number[i])
-			count2++;
+	for (int i = 0; i < 5; i++) {
+		sum += number[i] * number[i];
 	}
 
-	if (count1 == 8) 
-		printf("ascending");
-	else if (count2 == 8)
-		printf("descending");
-	else
-		printf("mixed");
+	sum %= 10;	
+	
+	printf("%d", sum);
 	
 	return 0;
 }
