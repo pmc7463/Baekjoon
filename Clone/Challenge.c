dd15e50468afdf01581f716f03a1bacc50aecce0
changstudent <akscjfals@naver.com>
2022-09-09 09:38:58 +0900

    [problem 2577] The number of numbers

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main() {
-	int number[4] = {0};
-	int sum = 0;
+	char sum[10] = {0};
+	int arr[10] = {0,};
+	int A, B, C, mul, length, temp;
+	
+	scanf("%d\n%d\n%d", &A, &B, &C);
+	
+	mul = A * B * C;
+	
+	sprintf(sum, "%d", mul);	//itoa 는 C 표준 함수가 아니며, 일부 C 구현체에만 지원합니다.
 
-	for (int i = 0; i < 5; i++) {
-		scanf("%d", &number[i]);
+	length = strlen(sum);
+	
+	for (int i = 0; i < length; i++) {
+		temp = mul % 10;
+		arr[temp]++;
+		mul /= 10;
+	}
+	for (int i = 0; i < 10; i++) {
+		printf("%d\n", arr[i]);
 	}
 
-	for (int i = 0; i < 5; i++) {
-		sum += number[i] * number[i];
-	}
-
-	sum %= 10;	
-	
-	printf("%d", sum);
-	
 	return 0;
 }
