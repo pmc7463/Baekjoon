fc2cade88771d6399587f547ab3e5ebd60ea0904
changstudent <akscjfals@naver.com>
2022-09-10 14:17:39 +0900

    [problem 2741] Number Counter

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,27 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 int main() {
-	char sum[10] = {0};
-	int arr[10] = {0,};
-	int A, B, C, mul, length, temp;
-	
-	scanf("%d\n%d\n%d", &A, &B, &C);
-	
-	mul = A * B * C;
-	
-	sprintf(sum, "%d", mul);	//itoa 는 C 표준 함수가 아니며, 일부 C 구현체에만 지원합니다.
+	int N;
 
-	length = strlen(sum);
-	
-	for (int i = 0; i < length; i++) {
-		temp = mul % 10;
-		arr[temp]++;
-		mul /= 10;
-	}
-	for (int i = 0; i < 10; i++) {
-		printf("%d\n", arr[i]);
+	scanf("%d", &N);
+
+	for (int i = 1; i <= N; i++) {
+		printf("%d\n", i);
 	}
 
 	return 0;
