6148d4e4514790c39460cab77c1996bac15352df
changstudent <akscjfals@naver.com>
2022-09-11 18:27:18 +0900

    [problem 2750] To sort numbers

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
 int main() {
-	int N;
+	int N, temp;
+	int arr[1000] = { 0 };
 
 	scanf("%d", &N);
 
-	for (int i = N; i >= 1; i--) {
-		printf("%d\n", i);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N - 1; j++) {
+			if (arr[j + 1] < arr[j]) {
+				temp = arr[j + 1];
+				arr[j + 1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < N; i++) {
+		printf("%d\n", arr[i]);
 	}
 
 	return 0;
