8b1828590b22cce67352d079825e6473fdb57193
changstudent <akscjfals@naver.com>
2022-09-11 19:41:40 +0900

    [problem 11653] Prime Factorization

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,28 +1,16 @@
 #include <stdio.h>
 
 int main() {
-	int N, temp;
-	int arr[1000] = { 0 };
+	int N;
 
 	scanf("%d", &N);
 
-	for (int i = 0; i < N; i++) {
-		scanf("%d", &arr[i]);
-	}
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N - 1; j++) {
-			if (arr[j + 1] < arr[j]) {
-				temp = arr[j + 1];
-				arr[j + 1] = arr[j];
-				arr[j] = temp;
-			}
+	for (int i = 2; i <= N; i++) {
+		while (N % i == 0) {
+			N /= i;
+			printf("%d\n", i); 
 		}
 	}
-
-	for (int i = 0; i < N; i++) {
-		printf("%d\n", arr[i]);
-	}
-
+	
 	return 0;
 }
