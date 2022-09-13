da6cae0dfbcb3ab445ad346d5d738dc99a956c64
changstudent <akscjfals@naver.com>
2022-09-13 21:43:07 +0900

    [problem 4948] Find the prime number sieve of Eratosthenes-2

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
-#define TEST 1000001
+#define TEST 246913
 
 int arr[TEST];  // 전역 변수로 배열을 선언하면 0으로 초기화 된다. 그리고 스택이 아닌 힙에 저장되기 때문에 크기에대해서 자유롭다
 
 int main(void)
 {
-    int min, max;
+    int num;
     int i, j;
-    
+    int count = 0;
+
     arr[0] = 1; // arr[i] = 1 이라는 것은 소수가 아니다라는 뜻
     arr[1] = 1;
 
@@ -19,11 +20,17 @@ int main(void)
                 arr[i] = 1;
     }
 
-    scanf("%d %d", &min, &max);
+    scanf("%d", &num);
     
-    for (i = min; i <= max; i++)
-        if (arr[i] == 0)
-            printf("%d\n", i);
-    
+    while (num != 0) {
+        count = 0;
+        for (i = num + 1; i <= num * 2; i++) {
+            if (arr[i] == 0)
+                count++;
+        }
+        printf("%d\n", count);
+        scanf("%d", &num);
+    }
+
     return 0;
 }
