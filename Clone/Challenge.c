b0748f624e6f786ac2b9246478a594d6747e206a
changstudent <akscjfals@naver.com>
2022-09-15 22:29:46 +0900

    [problem 9020] Goldbach conjecture

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,34 +1,33 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void insertSort(int num,int *arr) {
-    int i, j, key;
+int main(void) {
+    int T, N, i, j;
+    int arr[10001] = {0,};
 
-    for (i = 1; i < num; i++) {
-        key = arr[i];
-
-        for (j = i - 1; j >= 0 && arr[j] < key; j--) {
-            arr[j + 1] = arr[j];    // 한 칸 뒤로 이동
+    for (i = 2; i < 10001 / i; i++) {
+        if (!arr[i]) {
+            for (j = i * i; j < 10001; j += i)
+                if (j % i == 0)
+                    arr[j] = 1;            
         }
-        arr[j + 1] = key;   //알맞은 위치에 key삽입
     }
-}
 
-int main() {
-    int N, k, i;
-    int *arr;
+    scanf("%d", &T);
 
-    scanf("%d %d", &N, &k);
+    while (T--) {
+        scanf("%d", &N);
 
-    arr = (int *)malloc(sizeof(int)*N); // 그 길이에 맞춘 배열 생성
-    for (i = 0; i < N; i++) {
-
-        scanf("%d", &arr[i]);
+        for (i = N / 2; i > 1; i--) {
+            if (arr[i] == 1)
+                continue;
+            for (j = N / 2; j <= N; j++) {
+                if (arr[j] == 1)
+                    continue;
+                if (i + j == N) 
+                    goto OUT;
+            }
+        }
+        OUT: printf("%d %d\n", i , j); 
     }
-    
-    insertSort(N, arr);
-
-    printf("%d\n", arr[k - 1]);
-
     return 0;
 }
