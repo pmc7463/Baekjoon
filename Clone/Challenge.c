e05044e4442fc95155202a93aa9ff8fc1e10ce25
changstudent <akscjfals@naver.com>
2022-09-20 22:44:17 +0900

    [problem 11650] Aligning Coordinates

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,29 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int factorial(int num) {
-    int result = 1;
+typedef struct {
+    int x;
+    int y; 
+} coord;
 
-    for (int i = 1; i <= num; i++) {
-        result = result * i;
+int compare(const void *a, const void *b) {
+    coord A = *(coord *)a;  //coord형의 A로 입력받은 a를 사용할 수 있게 한다.
+    coord B = *(coord *)b;
+    if (A.x > B.x)  // 구조체의 x를 끌고와서 사용한다. 오름차순 정렬 
+        return 1;
+    else if (A.x == B.x) {   // x좌표가 같다면,
+        if (A.y > B.y)  // y좌표를 비교한다. 오름차순 정렬
+            return 1;
+        else
+            return -1;  
     }
-
-    return result;
+    return -1;
 }
 
-int main() {
-    int N, K;
-    int A, B, C;
-    int result;
+int main () {
+    int n, i = 0;
+    scanf("%d", &n);
 
-    scanf("%d", &N);
-    scanf("%d", &K);
+    coord arr[n];   // 구조체형으로 배열을 선언한다.
+    while (i < n) {
+        scanf("%d %d", &arr[i].x, &arr[i].y);
+        // 구조체 배열로 arr[0]의 x값, y값 각각 설정 하면선 간다.
+        i++;
+    }
 
-    A = factorial(N);
-    B = factorial(K);
-    C = factorial(N - K);
-
-    result = A / (B * C);
-    printf("%d\n", result);
-    
+    qsort(arr, n, sizeof(coord), compare);
+    i = 0;
+    while (i < n) {
+        printf("%d %d\n", arr[i].x, arr[i].y);
+        i++;
+    }
     return 0;
 }
