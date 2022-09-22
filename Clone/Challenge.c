3b10c7a2993360a553971d7d66b3b52f9bb4d729
changstudent <akscjfals@naver.com>
2022-09-22 21:03:24 +0900

    [problem 11651] Aligning Coordinates 2

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,44 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#define SIZE 100000
 
-char result[SIZE * 2];
-int stack[SIZE];
-int top = -1;
+typedef struct
+{
+    int x;
+    int y;
+} coord;    // coord형의 구조체 서언
+
+int compare(const void *a, const void *b) {
+    coord A = *(coord *)a;
+    coord B = *(coord *)b;
+
+    if (A.y > B.y)  // 구조체의 y를 끌고와서 사용한다. 오름차순 정렬 
+        return 1;
+    else if (A.y == B.y) {
+        if (A.x > B.x) 
+            return 1;
+        else
+            return -1;
+    }
+    return -1;
+}
 
 int main() {
-    int n, num = 1, i, idx = 0, result_idx = 0;
+    int n, i = 0;
+    
     scanf("%d", &n);
-    
-    int* arr = (int*)malloc(sizeof(int)*n);
-    for (i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-    
-    while(1) {
-        if (top == -1 || stack[top] < arr[idx]) {
-            stack[++top] = num++;
-            result[result_idx++] = '+';
-        }
 
-        else if (stack[top] == arr[idx]) {
-            top--;
-            result[result_idx++] = '-';
-            idx++;
-        }
-        else {
-            printf("NO");
-            free(arr);
-            return 0;
-        }
+    coord arr[n];
 
-        if (result_idx == n * 2)
-            break;
+    while (i < n) {
+        scanf("%d %d", &arr[i].x, &arr[i].y);
+        // 구조체배열로 arr[0]의 x값 , y값 각각 설정하면서 간다.
+        i++;
     }
-    for (i = 0; i < result_idx; i++)
-        printf("%c\n", result[i]);
-
-    free(arr);
-
+    qsort(arr, n, sizeof(coord), compare);
+    i = 0;
+    while (i < n) {
+        printf("%d %d\n", arr[i].x, arr[i].y);
+        i++;
+    }
     return 0;
 }
