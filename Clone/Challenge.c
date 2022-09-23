dd46f13412d43f836ac57c03a149ebca6d723ac2
changstudent <akscjfals@naver.com>
2022-09-23 23:02:44 +0900

    [problem 9012] Bracket

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,43 +1,34 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-
-typedef struct {
-    char str[51];
-    int len;
-} Str;
-
-int compare(const void *a, const void *b) {
-    Str s1 = *(Str*)a;
-    Str s2 = *(Str*)b;
-
-    if (s1.len < s2.len)
-        return -1;
-    else if (s1.len > s2.len) 
-        return 1;
-    return strcmp(s1.str, s2.str);
-}
 
 int main() {
-    int n, i;
-    int min = 0, max = 0;
+    int n, i, j, count;
+    int left, right;
+    char arr[51];
 
     scanf("%d", &n);
-
-    Str s[n];
-
     for (i = 0; i < n; i++) {
-        scanf("%s", s[i].str);
-        s[i].len = strlen(s[i].str);
-    }
+        count = 1;
+        left = 0;
+        right = 0;
 
-    qsort(s, n, sizeof(Str), compare);
+        scanf("%s", arr);
 
-    puts(s[0].str);
-
-    for (i = 1; i < n; i++) {
-        if (strcmp(s[i - 1].str, s[i].str))
-            puts(s[i].str);
+        for (j = 0; j < strlen(arr); j++) {
+            if (arr[j] == '(')
+                left++;
+            if (arr[j] == ')')
+                right++;
+            if (right > left) {
+                count = 0;
+                break;
+            }
+        }
+        if (count == 1 && left == right)
+            printf("YES\n");
+        else
+            printf("NO\n");
     }
     return 0;
 }
+
