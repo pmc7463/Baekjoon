6d338aeb1a2bc408257df1b326731fd7d6f91f85
changstudent <akscjfals@naver.com>
2022-09-28 23:25:51 +0900

    [problem 4101] It is big?

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,39 +1,14 @@
 #include <stdio.h>
-#include <string.h>
-
-int counter;
-
-int cnt() {
-    counter += 1;
-    return counter;
-}
-
-int recursion(const char* s, int l, int r) {
-    if (l >= r)
-        return 1;
-    else if (s[l] != s[r])
-        return 0;
-    else {
-        cnt();
-        return recursion(s, l + 1, r - 1); 
-    }
-}
-
-int isPalindrome(const char *s) {
-    return recursion(s, 0, strlen(s) - 1);
-}
 
 int main() {
-    int n, i;
-    char arr[1001] = {0,};
-    scanf("%d", &n);
+    int a, b, c;
 
-    for (i = 0; i < n; i++) {
-        scanf("%s", &arr);
-        printf("%d ", isPalindrome(arr));
-        printf("%d\n", cnt());
-        counter = 0;
-    }
+    do {
+        scanf("%d %d", &a, &b);
+        if (a == 0 && b == 0)
+            break;
+        c = a > b? printf("Yes\n") : printf("No\n");
+    } while (a > 0 && b > 0);
 
     return 0;
 }
