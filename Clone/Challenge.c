416b79a8382a2ecb39b8f7a773486aa7ce4ba87e
changstudent <akscjfals@naver.com>
2022-10-03 23:04:56 +0900

    [problem 1373] Binary and Octal numbers

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,22 +1,16 @@
 #include <stdio.h>
-
-int compare(int a, int b, int c, int d) {
-    int sum = 0;
-    sum = a + b + c + d;
-    return sum;
-}
+#include <string.h>
+char a[1000003] = {'0', '0'};
 
 int main() {
-    int a1, a2, a3, a4;
-    int b1, b2, b3, b4;
+    scanf("%s", a + 2);
 
-    scanf("%d %d %d %d", &a1, &a2, &a3, &a4);
-    scanf("%d %d %d %d", &b1, &b2, &b3, &b4);
+    int alen = strlen(a);
+    int k = 0;
 
-    if (compare(a1, a2, a3, a4) >= compare(b1, b2, b3, b4))
-        printf("%d\n", compare(a1, a2, a3, a4));
-    else
-        printf("%d\n", compare(b1, b2, b3, b4));
+    for (int i = alen % 3; i < alen; i = i + 3) {
+        printf("%d", (a[i] - '0') * 4 + (a[i + 1] - '0') * 2 + (a[i + 2] - '0'));
+    }
         
     return 0;
 }
