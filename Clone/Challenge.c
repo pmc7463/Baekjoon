5ef7976084303225457ab69d90183836743916a4
changstudent <akscjfals@naver.com>
2022-10-05 22:24:20 +0900

    [problem 10039] An average score

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -2,7 +2,22 @@
 
 int main() {
     
-    printf("The world says hello!\n");
+    int arr[4] = {0,};
+    int sum;
 
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+        if (arr[i] < 40)
+            arr[i] = 40;
+    }
+
+    for (int i = 0; i < 5; i++) {
+        sum += arr[i];
+    }
+
+    sum /= 5;
+
+    printf("%d", sum);
+    
     return 0;
 }
