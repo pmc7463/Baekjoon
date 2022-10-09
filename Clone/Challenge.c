8f2a8496bc741906498b43b06a06f6f57adab60a
changstudent <akscjfals@naver.com>
2022-10-09 23:06:48 +0900

    test

--- Clone/Challenge.c
+++ Clone/Challenge.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    char arr1[1001] = {0,};
-    char arr2[1001] = {0,};
-
-    scanf("%s", &arr1);
-    scanf("%s", &arr2);
-    
-    if (strlen(arr1) >= strlen(arr2))
-        printf("go\n");
-    else
-        printf("no\n");
-    
+    printf("test");
     return 0;
 }
