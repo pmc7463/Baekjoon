b5598a63dc1378c581808a151dc44bd8ba20e95a
changstudent <akscjfals@naver.com>
2022-10-12 07:10:05 +0900

    [problem 15733] Who am I?

--- Challenge.c
+++ Challenge.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int input;
-
-    scanf("%d", &input);
-
-    for (int i = 1; i <= input; i++) {
-        for (int k = input - i; k > 0; k--) {
-            printf(" ");
-        }
-
-        for (int j = 0; j < i; j++) {
-            printf("*");
-        }
-
-        for (int p = 1; p < i; p++) {   // j의 별이 끝나면 p의 별 시작, 첫번째는 실행 하면 안됨
-            printf("*");
-        }
-        printf("\n");
-    }
+    printf("I'm Sexy");
 
     return 0;
 }
