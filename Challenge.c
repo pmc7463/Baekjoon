8477181c4182d1d380007b238e694fe5dc7b163b
changstudent <akscjfals@naver.com>
2022-10-13 23:30:01 +0900

    [problem 8370] Plane

--- Challenge.c
+++ Challenge.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("I'm Sexy");
+    int B1,B2,E1,E2, airplane;
+
+    scanf("%d %d %d %d", &B1, &B2, &E1, &E2);
+
+    airplane = B1 * B2 + E1 * E2;
+
+    printf("%d\n", airplane);
 
     return 0;
 }
