1669912434ad075332737114b879986569fe4f13
changstudent <akscjfals@naver.com>
2022-10-22 23:13:45 +0900

    [problem 1934] Minimum common multiple

--- Challenge.c
+++ Challenge.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
 
+int gcd(int x, int y) {
+    int temp;
+
+    while (y > 0) {
+        temp = x;
+        x = y;
+        y = temp % y;
+    }
+    return x;
+}
+
 int main() {
     int a , b;
-    scanf("%d %d", &a, &b);
-    printf("%d\n", a + b);
-    printf("%d\n", a - b);
-    printf("%d\n", a * b);
+    int input;
 
+    scanf("%d", &input);
+
+    for (int i = 0; i < input; i++) {
+        scanf("%d %d", &a, &b);
+
+        printf("%d\n", (a * b) / gcd(a, b));
+    }
     return 0;
 }
