94de309ae6f3077efd98c5042b5f9e39ff78aa35
changstudent <akscjfals@naver.com>
2022-10-25 17:36:47 +0900

    [problem 10808] The number of alphabets

--- Challenge.c
+++ Challenge.c
@@ -2,22 +2,16 @@
 
 int main(void)
 {
-    char arr[4] = "";   // arr 전부 0으로 초기화
-    int count = 0;
-    int i = 0;
-    int temp;
-    gets(arr);
-
-    while (arr[i++] > 0)
-        count++;
-    
-    for (i = 0; i < count / 2; i++) {
-        temp = arr[i];
-        arr[i] = arr[count - i - 1];
-        arr[count - i - 1] = temp;
+    char str;
+    int arr[27] = {0,};
+    while (scanf("%c", &str) != EOF) {
+        if (97 <= str && str <= 122)
+            arr[str - 97]++;
+        else
+            break;
     }
 
-    printf("%s", arr);
-    
+    for (int i = 0; i < 26; i++) 
+        printf("%d ", arr[i]);
     return 0;
 }
