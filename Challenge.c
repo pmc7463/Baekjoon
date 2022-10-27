d9827b6585947bd13091dd1b68645273adc11d95
changstudent <akscjfals@naver.com>
2022-10-27 21:14:53 +0900

    [problem 5554] The way to an errand

--- Challenge.c
+++ Challenge.c
@@ -2,11 +2,18 @@
 
 int main(void)
 {
-    int input;
+    int time[5] = {0,};
+    int sum = 0;
+    int clock;
 
-    scanf("%X", &input);
+    for (int i = 0; i < 4; i++) {
+        scanf("%d", &time[i]);
+        sum += time[i];
+    }
+    
+    clock = sum / 60;
+    sum %= 60;
 
-    printf("%d\n", input);
-
+    printf("%d\n%d\n", clock, sum);
     return 0;
 }
