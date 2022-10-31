2c3381291582554929645d70ab616ea8d9db1855
changstudent <akscjfals@naver.com>
2022-10-31 17:35:06 +0900

    [problem 9316] Hello Judge

--- Challenge.c
+++ Challenge.c
@@ -1,26 +1,14 @@
 #include <stdio.h>
 
-int main(void){
-    int i, j, k, input;
+int main(void)
+{
+    int input;
 
     scanf("%d", &input);
 
-    for (i = input; i > 0; i--) {
-        for (j = 1; j <= input - i; j++)
-            printf(" ");
-        for (k = (i*2)-1; k > 0; k--)
-            printf("*");
-        printf("\n");
+    for (int i = 1; i <= input; i++)
+    {
+        printf("Hello World, Judge %d!\n", i);
     }
-
-
-    for (i = 2; i <= input; i++) {
-        for (j = input; j - i> 0; j--)
-            printf(" ");
-        for (k = 1; k <= (i*2)-1; k++)
-            printf("*");
-        printf("\n");
-    }
-
     return 0;
 }
