2d933739b28934a26ded0b0bdbcab9b5975e663f
changstudent <akscjfals@naver.com>
2022-11-01 22:52:10 +0900

    [problem 2845] After the party

--- Challenge.c
+++ Challenge.c
@@ -2,24 +2,15 @@
 
 int main(void)
 {
-    int man, area;
-    int arr[5] = {0,};
-    int temp[5] = {0,};
+    int M, F;
 
-    scanf("%d %d", &man, &area);
-
-    for (int i = 0; i < 5; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    man *= area;
-
-    for (int i = 0; i < 5; i++) {
-        temp[i] = arr[i] - man;
-    }
-
-    for (int i = 0; i < 5; i++) {
-        printf("%d ", temp[i]);
+    while (1) {
+        scanf("%d %d", &M, &F);
+        
+        if ((M != 0) && (F != 0)) 
+            printf("%d\n", M + F);
+        else
+            break;
     }
     return 0;
 }
