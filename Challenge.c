d5b90f68fa5569faea0a61b3fed54b112d078049
changstudent <akscjfals@naver.com>
2022-11-23 23:19:53 +0900

    [problem 10807] Counting the number of Counts

--- Challenge.c
+++ Challenge.c
@@ -1,32 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int x[4], y[4];
+    int N, arr[100], goal, temp = 0;
 
-    for (int i = 0; i < 3; i++) {
-        scanf("%d %d", &x[i], &y[i]);
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++) {
+        scanf("%d", &arr[i]);
     }
 
-    if (x[0] == x[1])
-        x[3] = x[2];
+    scanf("%d", &goal);
 
-    else if (x[0] == x[2])
-        x[3] = x[1];
+    for (int i = 1; i <= N; i++) {
+        if (goal == arr[i])
+            temp++;          
+    }
 
-    else
-        x[3] = x[0];
-    
-
-    if (y[0] == y[1])
-        y[3] = y[2];
-
-    else if (y[0] == y[2])
-        y[3] = y[1];
-
-    else
-        y[3] = y[0];
-    
-    printf("%d %d\n", x[3], y[3]);
+    printf("%d\n", temp);
 
     return 0;
 }
