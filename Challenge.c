e1f6f7f60e4dfdc77aa08bbe7013bf204948e2e0
changstudent <akscjfals@naver.com>
2022-11-24 21:52:31 +0900

    [problem 2798] Black Jack

--- Challenge.c
+++ Challenge.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
 int main() {
-    int N, arr[100], goal, temp = 0;
+    int N, M, MAX = 0, sum = 0;
+    int arr[100];
 
-    scanf("%d", &N);
+    scanf("%d %d", &N, &M);
 
-    for (int i = 1; i <= N; i++) {
+    for (int i = 0; i < N; i++) {
         scanf("%d", &arr[i]);
     }
 
-    scanf("%d", &goal);
-
-    for (int i = 1; i <= N; i++) {
-        if (goal == arr[i])
-            temp++;          
+    for (int i = 0; i < N; i++) {
+        for (int j = i + 1; j < N; j++) {
+            for (int k = j + 1; k < N; k++) {
+                sum = arr[i] + arr[j] + arr[k];
+                if (sum > MAX && sum <= M)
+                    MAX = sum;
+            }
+        }
     }
 
-    printf("%d\n", temp);
+    printf("%d\n", MAX);
 
     return 0;
 }
