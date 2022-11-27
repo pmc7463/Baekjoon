108a3ac957f1630cb0938be862e79421b1d5a774
changstudent <akscjfals@naver.com>
2022-11-27 18:53:03 +0900

    [problem 3053] Taxi cab geometry

--- Challenge.c
+++ Challenge.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
+#define PI 3.14159265359
 int main() {
-    printf("       _.-;;-._\n");
-    printf("'-..-'|   ||   |\n");
-    printf("'-..-'|_.-;;-._|\n");
-    printf("'-..-'|   ||   |\n");
-    printf("'-..-'|_.-''-._|\n");
+    int radius;
+
+    scanf("%d", &radius);
+
+    printf("%0.6lf\n%0.6lf\n", (double)radius*radius*PI, (double)radius*radius*2);
+
     return 0;
 }
