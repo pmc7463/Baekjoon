20ab0a413e3351c0c2ff2bea5516bb1c849fd18a
changstudent <akscjfals@naver.com>
2022-11-28 23:02:11 +0900

    [problem 2309] The Seven Dwarfs

--- Challenge.c
+++ Challenge.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
-#define PI 3.14159265359
+#include <stdlib.h>
+
 int main() {
-    int radius;
+    int arr[9], height[7];
+    int sum = 0, over = 0;
+    int x = 0, y = 0, temp, i, j;
 
-    scanf("%d", &radius);
+    for (int i = 0; i < 9; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
 
-    printf("%0.6lf\n%0.6lf\n", (double)radius*radius*PI, (double)radius*radius*2);
+    over = sum - 100;
 
+    for (int i = 0; i < 9; i++) {
+        for (int j = i + 1; j < 9; j++) {
+            if ((arr[i] + arr[j]) == over) {
+                x = i;
+                y = j;
+                break;
+            }
+        }
+    }
+
+    j = 0;
+    for (int i = 0; i < 9; i++) {
+        if (i != x && i != y) {
+            height[j] = arr[i];
+            j++;
+        }
+    }
+    
+    for (int i = 0; i < 7; i++) {
+        for (int j = i + 1; j < 7; j++) {
+            if (height[i] > height[j]) {
+                temp = height[i];
+                height[i] = height[j];
+                height[j] = temp;
+            }
+        }
+    }
+
+    for (int i = 0; i < 7; i++) {
+        printf("%d\n", height[i]);
+    }
     return 0;
 }
