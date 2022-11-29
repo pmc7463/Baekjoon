8e08a7c5a04081fd446b7bc60fe94dc417321497
changstudent <akscjfals@naver.com>
2022-11-29 21:22:31 +0900

    [problem 11948] Select a subject

--- Challenge.c
+++ Challenge.c
@@ -1,48 +1,36 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    int arr[9], height[7];
-    int sum = 0, over = 0;
-    int x = 0, y = 0, temp, i, j;
+    int a[4], b[2];
+    int temp, bmax, sum= 0;
 
-    for (int i = 0; i < 9; i++) {
-        scanf("%d", &arr[i]);
-        sum += arr[i];
+    for (int i = 0; i < 4; i++) {
+        scanf("%d", &a[i]);
+    }
+    for (int i = 0; i < 2; i++) {
+        scanf("%d", &b[i]);
     }
 
-    over = sum - 100;
-
-    for (int i = 0; i < 9; i++) {
-        for (int j = i + 1; j < 9; j++) {
-            if ((arr[i] + arr[j]) == over) {
-                x = i;
-                y = j;
-                break;
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 4; j++) {
+            if (a[i] < a[j]) {
+                temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
             }
         }
     }
 
-    j = 0;
-    for (int i = 0; i < 9; i++) {
-        if (i != x && i != y) {
-            height[j] = arr[i];
-            j++;
-        }
-    }
-    
-    for (int i = 0; i < 7; i++) {
-        for (int j = i + 1; j < 7; j++) {
-            if (height[i] > height[j]) {
-                temp = height[i];
-                height[i] = height[j];
-                height[j] = temp;
-            }
-        }
+    if (b[0] >= b[1])
+        bmax = b[0];
+    else
+        bmax = b[1];
+
+    for (int i = 0; i < 3; i++) {
+        sum += a[i];
     }
 
-    for (int i = 0; i < 7; i++) {
-        printf("%d\n", height[i]);
-    }
+    printf("%d\n", sum + bmax);
+
     return 0;
 }
