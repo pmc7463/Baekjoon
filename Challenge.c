52a1d435f8b741f5d1103a8e36501c9f1f843de7
pmc7463 <akscjfals@naver.com>
2022-12-01 22:12:14 +0900

    [problem 5575] Time Card

--- Challenge.c
+++ Challenge.c
@@ -1,17 +1,66 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() {
-    char str[101];
-    int cnt = 0;
+    int start[18];
+    int finish[9];
 
-    scanf("%s", &str);
-
-    for (int i = 0; i < strlen(str); i++) {
-        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
-            cnt++;
+    for (int i = 0; i < 18; i++) {
+        scanf("%d", &start[i]);
     }
 
-    printf("%d\n", cnt);
+    if (start[5] < start[2]) {
+        finish[2] = start[5] + 60 - start[2];
+        start[4] = start[4] - 1;
+    }
+    else
+        finish[2] = start[5] - start[2];
+
+    if (start[4] < start[1]) {
+        finish[1] = start[4] + 60 - start[1];
+        start[3] = start[3] - 1;
+    }
+    else 
+        finish[1] = start[4] - start[1];
+    
+    finish[0] = start[3] - start[0];
+    
+    
+    if (start[11] < start[8]) {
+        finish[5] = start[11] + 60 - start[8];
+        start[10] = start[10] - 1;
+    }
+    else
+        finish[5] = start[11] - start[8];
+
+    if (start[10] < start[7]) {
+        finish[4] = start[10] + 60 - start[7];
+        start[9] = start[9] - 1;
+    }
+    else 
+        finish[4] = start[10] - start[7];
+    
+    finish[3] = start[9] - start[6];
+
+    
+    if (start[17] < start[14]) {
+        finish[8] = start[17] + 60 - start[14];
+        start[16] = start[16] - 1;
+    }
+    else
+        finish[8] = start[17] - start[14];
+
+    if (start[16] < start[13]) {
+        finish[7] = start[16] + 60 - start[13];
+        start[15] = start[15] - 1;
+    }
+    else 
+        finish[7] = start[16] - start[13];
+    
+    finish[6] = start[15] - start[12];
+
+    printf("%d %d %d\n", finish[0], finish[1], finish[2]);
+    printf("%d %d %d\n", finish[3], finish[4], finish[5]);
+    printf("%d %d %d\n", finish[6], finish[7], finish[8]);
+
     return 0;
 }
