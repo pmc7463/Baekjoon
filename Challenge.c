ccca30d05c02c1fef38cb5e8405c20beb978ca9b
akscjfals@naver.com <akscjfals@naver.com>
2022-12-06 20:13:25 +0900

    [problem 2490] Playing Yut

--- Challenge.c
+++ Challenge.c
@@ -1,13 +1,31 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int clothes;
-
-    scanf("%d", &clothes);
-
-    if (1 <= clothes && clothes <= 2017)
-        printf("%d\n", clothes * (clothes - 1));
-
+int main() {
+    int yut = 0, sum = 0;
+    
+    for (int j = 0; j < 3; j++) {
+        sum = 0;
+        for (int i = 0; i < 4; i++) {
+            scanf("%d ", &yut);
+            if (yut == 0) {
+                sum++;
+            }
+        }
+        if (sum == 1) {
+            printf("A\n");
+        }
+        else if (sum == 2) {
+            printf("B\n");
+        }
+        else if (sum == 3) {
+            printf("C\n");
+        }
+        else if (sum == 4) {
+            printf("D\n");
+        }
+        else if (sum == 0) {
+            printf("E\n");
+        }
+    }
     return 0;
 }
