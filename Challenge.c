a017ad640ae5be6b100f4a9ac3d4be273d1736f3
Min Fe 97 <77577896+pmc7463@users.noreply.github.com>
2022-12-09 11:16:44 +0900

    [problem 7568] Big body

--- Challenge.c
+++ Challenge.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
-#define PI 3.141592
 
 int main() {
-    int rectangle, circle;
+    int human, cnt;
+    int body[50][2];
 
-    scanf("%d %d", &rectangle, &circle);
+    scanf("%d", &human);
+​
+​
+    for (int i = 0; i < human; i++) {
+        scanf("%d %d", &body[i][0], &body[i][1]);
+    }
 
-    printf("%0.6f\n", (double)rectangle * 2 + circle * 2 * PI);
+    for (int i = 0; i < human; i++) {
+        cnt = 0;
+        for (int j = 0; j < human; j++) {
+            if (body[i][0] < body[j][0] && body[i][1] < body[j][1]) {
+                cnt++;
+            }
+        }
+        printf("%d ", ++cnt);
+    }
+    
 
     return 0;
-}
+}​
