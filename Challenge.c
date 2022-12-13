8fa3e69d569d646730a2f5b130fd91ff840f1c4d
akscjfals@naver.com <akscjfals@naver.com>
2022-12-13 16:24:23 +0900

    [problem 1924] Year 2007

--- Challenge.c
+++ Challenge.c
@@ -2,15 +2,52 @@
 
 int main(void)
 {
-    int kill, assist, death;
+    int month, day, date = 0;
 
-    scanf("%d/%d/%d", &kill, &death, &assist);
+    scanf("%d %d", &month, &day);
 
-    if (kill + assist < death || death == 0) {
-        printf("hasu\n");
+    if (month != 1) {
+        for (int i = 1; i < month; i++) {
+            if (i == 2)
+                date += 28;
+            else if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12)
+                date += 31;
+            else 
+                date += 30;
+        }
     }
-    else
-        printf("gosu\n");
 
+    if (month == 1) {
+        for (int i = 1; i <= day; i++)
+            date += 1;
+    }
+    else {
+        for (int i = 1; i <= day; i++)
+            date += 1;
+    }
+
+    switch(date % 7) {
+        case 0:
+            printf("SUN");
+            break;
+        case 1:
+            printf("MON");
+            break;
+        case 2:
+            printf("TUE");
+            break;
+        case 3:
+            printf("WED");
+            break;
+        case 4:
+            printf("THU");
+            break;
+        case 5:
+            printf("FRI");
+            break;
+        default:
+            printf("SAT");
+            break;
+    }
     return 0;
 }
