b66c1ad4e5407abdcd84ca2a6484c79fe246153b
akscjfals@naver.com <akscjfals@naver.com>
2022-12-14 23:53:30 +0900

    [problem 19698] The sale of a house

--- Challenge.c
+++ Challenge.c
@@ -2,52 +2,13 @@
 
 int main(void)
 {
-    int month, day, date = 0;
+    int cow, width, height, length, answer;
 
-    scanf("%d %d", &month, &day);
+    scanf("%d %d %d %d", &cow, &width, &height, &length);
 
-    if (month != 1) {
-        for (int i = 1; i < month; i++) {
-            if (i == 2)
-                date += 28;
-            else if (i == 1 || i == 3 || i == 5 || i == 7 || i == 8 || i == 10 || i == 12)
-                date += 31;
-            else 
-                date += 30;
-        }
-    }
+    answer = (width / length) * (height / length);
 
-    if (month == 1) {
-        for (int i = 1; i <= day; i++)
-            date += 1;
-    }
-    else {
-        for (int i = 1; i <= day; i++)
-            date += 1;
-    }
-
-    switch(date % 7) {
-        case 0:
-            printf("SUN");
-            break;
-        case 1:
-            printf("MON");
-            break;
-        case 2:
-            printf("TUE");
-            break;
-        case 3:
-            printf("WED");
-            break;
-        case 4:
-            printf("THU");
-            break;
-        case 5:
-            printf("FRI");
-            break;
-        default:
-            printf("SAT");
-            break;
-    }
+    printf("%d\n", cow < answer ? cow : answer);
+    
     return 0;
 }
