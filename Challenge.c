44077748072d771947fef479b215fa361c68eb78
akscjfals@naver.com <akscjfals@naver.com>
2022-12-15 17:03:58 +0900

    [problem 2748] Fibonacci 2

--- Challenge.c
+++ Challenge.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-int main(void)
-{
-    int cow, width, height, length, answer;
+int main(){
+    int n;
+    scanf("%d", &n);
+    long long arr[91] = {0, 1};
 
-    scanf("%d %d %d %d", &cow, &width, &height, &length);
+    for (int i = 2; i <= n; i++)
+        arr[i] = arr[i - 1] + arr[i - 2];
 
-    answer = (width / length) * (height / length);
+    printf("%lld", arr[n]);
 
-    printf("%d\n", cow < answer ? cow : answer);
-    
     return 0;
 }
