3ab7ac53e58203a2a1e5aa7a137672b3ca9cec11
akscjfals@naver.com <akscjfals@naver.com>
2022-12-17 23:08:05 +0900

    [problem 9610] A quadrant

--- Challenge.c
+++ Challenge.c
@@ -1,27 +1,40 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-	int max = 0 , sum = 0, line;
-	int score[5][4];
-	
-    for(int i = 0; i < 5; i++){
-		for(int j = 0; j < 4; j++){
-			scanf("%d", &score[i][j]);
-		}
+	int num, Q1 = 0, Q2 = 0, Q3 = 0, Q4 = 0, AXIS = 0;
+	int* x;
+	int* y;
+
+	scanf("%d", &num);
+	x = (int*)malloc(sizeof(int) * num);
+	y = (int*)malloc(sizeof(int) * num);	
+
+	for (int i = 0; i < num; i++) {
+		scanf("%d %d", &x[i], &y[i]);
 	}
 
-	for(int i = 0; i < 5; i++){
-		for(int j = 0; j < 4; j++){
-			sum += score[i][j];
-		}
-        
-		if(max < sum){
-			max = sum;
-			line = i + 1;
-		}
-		sum = 0;
+	for (int i = 0; i < num; i++) {
+		if (x[i] > 0 && y[i] > 0)
+			Q1++;
+		if (x[i] > 0 && y[i] < 0)
+			Q4++;
+		if (x[i] < 0 && y[i] > 0)
+			Q2++;
+		if (x[i] < 0 && y[i] < 0)
+			Q3++;
+		if (x[i] == 0 || y[i] == 0)
+			AXIS++;
 	}
-	printf("%d %d", line, max);
+
+	printf("Q1: %d\n", Q1);
+	printf("Q2: %d\n", Q2);
+	printf("Q3: %d\n", Q3);
+	printf("Q4: %d\n", Q4);
+	printf("AXIS: %d\n", AXIS);
+
+	free(x);
+	free(y);
 	return 0; 
 }
