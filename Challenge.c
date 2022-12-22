f796337cbdab0e4ee58f36dadae60a533d7ccd6b
Min Fe 97 <77577896+pmc7463@users.noreply.github.com>
2022-12-22 21:25:12 +0900

    [problem 2523] Expressing the stars 13

--- Challenge.c
+++ Challenge.c
@@ -3,25 +3,15 @@
 int main() {
 	int n;
 	scanf("%d", &n);
-	int i, j;
-	for (i = 1; i <= n; i++) {
-		for (j = 1; j <= n - i; j++) {
-			printf(" ");
-		}
-		for (j = 1; j <= i; j++) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j <i+1; j++) {
 			printf("*");
-		}
-		printf("\n");
+		}printf("\n");
 	}
-	
-	for (i = n - 1; i > 0; i--) {
-		for (j = 1; j <= n - i; j++) {
-			printf(" ");
-		}
-		for (j = 1; j <= i; j++) {
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < n - i; j++) {
 			printf("*");
-		}
-		printf("\n");
+		}printf("\n");
 	}
 	return 0;
 }
