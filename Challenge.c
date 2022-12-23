f4d62d2126043781cd2b370132a053955a938d77
Min Fe 97 <77577896+pmc7463@users.noreply.github.com>
2022-12-24 00:30:50 +0900

    [problem 10990] Expressing the stars 15 ​

--- Challenge.c
+++ Challenge.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 ​
-int main() {
-	int n;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j <i+1; j++) {
-			printf("*");
-		}printf("\n");
-	}
-	for (int i = 1; i < n; i++) {
-		for (int j = 0; j < n - i; j++) {
-			printf("*");
-		}printf("\n");
-	}
-	return 0;
+int main()
+{
+   int n;
+   scanf("%d", &n);
+   // 첫번째 줄
+   for (int i = 1; i <= n - 1; i++)
+   {
+       printf(" ");
+   }
+   printf("*\n");
+   // 2 ~ n번째 줄
+   for (int i = 2; i <= n; i++)
+   {
+       // 외부 공백
+       for (int j = 1; j <= n - i; j++)
+       {
+           printf(" ");
+       }
+       printf("*");
+       // 내부 공백
+       for (int k = 1; k <= (i - 1) * 2 - 1; k++)
+       {
+           printf(" ");
+       }
+       printf("*\n");
+   }
+   return 0;
 }
