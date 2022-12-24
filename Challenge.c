e4c2f7492a00b28016f82759405836549ee36d4c
changstudent <akscjfals@naver.com>
2022-12-24 23:36:53 +0900

    [problem 5086] Multiple and Factor

--- Challenge.c
+++ Challenge.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
-​
-int main()
-{
-   int n;
-   scanf("%d", &n);
-   // 첫번째 줄
-   for (int i = 1; i <= n - 1; i++)
-   {
-       printf(" ");
-   }
-   printf("*\n");
-   // 2 ~ n번째 줄
-   for (int i = 2; i <= n; i++)
-   {
-       // 외부 공백
-       for (int j = 1; j <= n - i; j++)
-       {
-           printf(" ");
-       }
-       printf("*");
-       // 내부 공백
-       for (int k = 1; k <= (i - 1) * 2 - 1; k++)
-       {
-           printf(" ");
-       }
-       printf("*\n");
-   }
-   return 0;
+
+int main() {
+    int num1, num2;
+
+    while(1) {
+        scanf("%d %d", &num1, &num2);
+        if (num1 != 0 && num2 != 0) {
+            if (num2 > num1)
+                if (num2 % num1 == 0) 
+                    printf("factor\n");
+                else
+                    printf("neither\n");
+            else if (num1 > num2)
+                if (num1 % num2 == 0)
+                    printf("multiple\n");
+                else
+                    printf("neither\n");
+        }
+        else
+            break;
+    }
+    return 0;
 }
