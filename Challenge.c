23a3a172b0cdeb479fc27dc0bf69fbb320c3ab60
changstudent <akscjfals@naver.com>
2022-12-25 19:45:44 +0900

    [problem 5585] Change

--- Challenge.c
+++ Challenge.c
@@ -1,24 +1,47 @@
 #include <stdio.h>
 
 int main() {
-    int num1, num2;
+    int money, counter = 0, change;
 
-    while(1) {
-        scanf("%d %d", &num1, &num2);
-        if (num1 != 0 && num2 != 0) {
-            if (num2 > num1)
-                if (num2 % num1 == 0) 
-                    printf("factor\n");
-                else
-                    printf("neither\n");
-            else if (num1 > num2)
-                if (num1 % num2 == 0)
-                    printf("multiple\n");
-                else
-                    printf("neither\n");
+    scanf("%d", &money);
+
+    change = 1000 - money;
+    
+    while (change != 0)
+    {
+        if (change >= 500)
+        {
+            change -= 500;
+            counter++;
         }
-        else
-            break;
+        if (change >= 100 && change < 500)
+        {
+            change -= 100;
+            counter++;
+        }
+        if (change >= 50 && change < 100)
+        {
+            change -= 50;
+            counter++;
+        }
+        if (change >= 10 && change < 50)
+        {
+            change -= 10;
+            counter++;
+        }
+        if (change >= 5 && change < 10)
+        {
+            change -= 5;
+            counter++;
+        }
+        if (change >= 1 && change < 5)
+        {
+            change -= 1;
+            counter++;
+        }
     }
+
+    printf("%d\n", counter);
+
     return 0;
 }
