d04162ba674b30cc2e2e3ccd1442399ac2aeeecd
akscjfals@naver.com <akscjfals@naver.com>
2022-12-31 20:24:58 +0900

    [problem 1436] Find number 666

--- python.py
+++ python.py
@@ -1,18 +1,10 @@
-from math import sqrt
-
 num = int(input())
+first = 666     # 처음 666인 수
 
-for i in range(num) :
-    secret = input()
-    
-    save = int(sqrt(len(secret)))
-    
-    origin = ''
-    
-    for j in range (save-1, -1, -1):
-        for k in range(0, len(secret), save) :
-            word = secret[k : k + save]
-            origin += word[j]
-            
-    print(origin)
-            
+while num != 0 :    # num이 0아니면 계속 반복
+    if '666' in str(first): # 만약 666이란 문자열이 문자열(first)안에 있으면
+        num = num - 1   # -1
+        if num == 0:    # 만약 0이면
+            break       # 반목문 탈출
+    first = first + 1   # +1
+print(first)
