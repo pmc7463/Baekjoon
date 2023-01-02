e5008d937c7593c04c0edcab159543de683c9edf
akscjfals@naver.com <akscjfals@naver.com>
2023-01-02 20:44:56 +0900

    [problem 1037] Divisor

--- Challenge.c
+++ Challenge.c
@@ -1,42 +1,21 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 
 int main(void) {
-	char num1[8], num2[8];
-	int min, max;
+	int num1, num2;
+	long long max = 0, min = 1000001;
 
-	scanf("%s %s", num1, num2);
+	scanf("%d", &num1);
 
-	for (int i = 0; i < strlen(num1); i++) {
-		if (num1[i] == '6') {
-			num1[i] = '5';
-		}
+	for (int i = 0; i < num1; i++) {
+		scanf("%d", &num2);
+
+		if (num2 > max)
+			max = num2;
+		if (num2 < min)
+			min = num2;
 	}
 
-	for (int i = 0; i < strlen(num2); i++) {
-		if (num2[i] == '6') {
-			num2[i] = '5';
-		}
-	}
-
-	min = atoi(num1) + atoi(num2);
-
-	for (int i = 0; i < strlen(num1); i++) {
-		if (num1[i] == '5') {
-			num1[i] = '6';
-		}
-	}
-
-	for (int i = 0; i < strlen(num2); i++) {
-		if (num2[i] == '5') {
-			num2[i] = '6';
-		}
-	}
-
-	max = atoi(num1) + atoi(num2);
-
-	printf("%d %d\n", min, max);
+	printf("%lld\n", max * min);
 
 	return 0; 
 }
