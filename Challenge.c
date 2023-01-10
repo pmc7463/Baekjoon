495b1110fdb625a053ba162b0857c237a78449bf
akscjfals@naver.com <akscjfals@naver.com>
2023-01-10 17:23:08 +0900

    [problem 10988] The palindrome

--- Challenge.c
+++ Challenge.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
-int main(void) {
-	int repetion, variable, degree, temp = 0;
+int main() {
+	char str[100];
+	int result = 1;
+	
+	scanf("%s", str);
 
-	scanf("%d", &repetion);
-
-	for (int j = 0; j < repetion; j++) {
-		scanf("%d %d", &variable, &degree);
-		temp = variable;
-		for (int i = 1; i < degree; i++) {
-			temp = variable * temp % 10;
-		}
-		if (temp % 10 == 0)
-			printf("10\n");
-		else
-			printf("%d\n", temp % 10);
+	for (int i = 0; i < strlen(str) / 2; i++) {
+		if (str[i] != str[strlen(str) - 1 - i])
+			result = 0;
 	}
+
+	if (result == 1)
+		printf("1");
+	else
+		printf("0");
+
 	return 0;
 }
