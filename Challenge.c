6bff41ad24d30f8c872421dffc62f7653da0f5f7
akscjfals@naver.com <akscjfals@naver.com>
2023-01-13 11:49:28 +0900

    [problem 2669] Find the area of the union of four rectangles

--- Challenge.c
+++ Challenge.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
 int main() {
-	int chocolate, width, height;
+	int rectangle[101][101] = {0,};
+	int cnt = 0;
+	int x1, y1, x2, y2;
 
-	scanf("%d %d", &width, &height);
+	for (int i = 0; i < 4; i++) {
+		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+		for (int j = x1; j < x2; j++) {
+			for (int k = y1; k < y2; k++) {
+				rectangle[j][k] = 1;
+			}
+		}
+	}
 
-	printf("%d\n", width * height - 1);
+	for (int i = 1; i < 101; i++) {
+		for (int j = 1; j < 101; j++) {
+			if (rectangle[i][j] == 1)
+				cnt++;
+		}
+	}
+
+	printf("%d\n", cnt);
 
 	return 0;
 }
