d39bb0940199f8f04f322e0d0e3d2aa38fc573db
changstudent <akscjfals@naver.com>
2023-01-14 19:36:57 +0900

    [problem 1236] Protecting the Castle

--- Challenge.c
+++ Challenge.c
@@ -1,27 +1,43 @@
 #include <stdio.h>
 
 int main() {
-	int rectangle[101][101] = {0,};
-	int cnt = 0;
-	int x1, y1, x2, y2;
+	int row, col, tempRow, tempCol;
+    char guard[50][50] = {0};
 
-	for (int i = 0; i < 4; i++) {
-		scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
-		for (int j = x1; j < x2; j++) {
-			for (int k = y1; k < y2; k++) {
-				rectangle[j][k] = 1;
-			}
-		}
-	}
+    scanf("%d %d", &row, &col);
+    getchar();
 
-	for (int i = 1; i < 101; i++) {
-		for (int j = 1; j < 101; j++) {
-			if (rectangle[i][j] == 1)
-				cnt++;
-		}
-	}
+    tempRow = row;
+    tempCol = col;
 
-	printf("%d\n", cnt);
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) 
+            guard[i][j] = getchar();
+        getchar();
+    }
 
-	return 0;
+    for (int i = 0; i < row; i++) {
+        for (int j = 0; j < col; j++) {
+            if (guard[i][j] == 'X') {
+                tempRow--;
+                break;
+            }   
+        }
+    }
+
+    for (int i = 0; i < col; i++) {
+        for (int j = 0; j < row; j++) {
+            if (guard[j][i] == 'X') {
+                tempCol--;
+                break;
+            }
+        }
+    }
+
+    if (tempRow > tempCol)
+        printf("%d\n", tempRow);
+    else
+        printf("%d\n", tempCol);
+
+    return 0;
 }
--- python.py
+++ python.py
@@ -1,16 +1,1 @@
-arr_A, arr_B = [], []
-
-n, m = map(int, input().split())
-
-for row in range(n):
-    row = list(map(int, input().split()))
-    arr_A.append(row)
-
-for row in range(n):
-    row = list(map(int, input().split()))
-    arr_B.append(row)
-
-for row in range(n):
-    for col in range(m):
-        print(arr_A[row][col] + arr_B[row][col], end = ' ')
-    print()
+print("Hello")
