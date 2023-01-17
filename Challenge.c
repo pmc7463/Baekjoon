6623a1062a0eafacc028aa8afff1c245ffd8b5ea
akscjfals@naver.com <akscjfals@naver.com>
2023-01-17 11:15:00 +0900

    [problem 1259] Palindrome Number

--- Challenge.c
+++ Challenge.c
@@ -1,70 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-void quicksort(int data[], int start, int end) {    // 매개변수로 배열, 시작, 끝 값 받음
-    if (start >= end) { //원소가 1개인 경우를 의미
-        return;
-    }
-
-    int key = start; // key는 첫번째 원소 (기준을 의미)
-    int i = start + 1;  //기준이 되는 숫자 바로 오른쪽을 말함.
-    int j = end;    //배열의 가장 끝을 말함.
-    int temp;   //swap을 위한 변수
-
-    while (i <= j) {    // 엇 갈리기 전까지 계속 반복
-        while (data[i] <= data[key]) {  // 첫번째 케이스 : 기준값보다 큰 수를 찾아가는 경우
-            i++;
-        }
-
-        while (data[j] > data[key] && j > start) {  // 두번째 케이스 : 기준값보다 작은 수를 찾아가는 경우
-            j--;
-        }
-
-        if ( i > j) {
-            temp = data[j];
-            data[j] = data[key];
-            data[key] = temp;
-        }
-
-        else {
-            temp = data[j];
-            data[j] = data[i];
-            data[i] = temp;
-        }
-    }
-    quicksort(data, start, j-1);
-    quicksort(data, j+1, end);
-}
+#include <string.h>
 
 int main() {
-    int* arr;
-    int* timeSum;
+    char strNumber[100000] = "";
+    int palindrome = 1;
 
-    int people, sum1 = 0, sum2 = 0;
+    while (1) {
+        scanf("%s", &strNumber);
 
-    scanf("%d", &people);
-
-    arr = malloc(sizeof(int) * people);
-    timeSum = malloc(sizeof(int) * people);
-
-    for (int i = 0; i < people; i++) {
-        scanf("%d", &arr[i]);
+        if (strNumber[0] != '0') {
+            for (int i = 0; i < strlen(strNumber)/2; i++) {
+                if (strNumber[i] != strNumber[strlen(strNumber) - 1 - i]) {
+                    palindrome = 0;
+                }
+            }
+            if (palindrome == 1)
+                printf("yes\n");
+            else {
+                printf("no\n");
+                palindrome = 1;
+            }
+        }
+        else
+            break;
     }
-
-    quicksort(arr, 0, people-1);
-    
-    for (int i = 0; i < people; i++) {
-        sum1 += arr[i];
-        timeSum[i] = sum1;
-    }
-
-    for (int i = 0; i < people; i++) {
-        sum2 += timeSum[i];
-    }
-
-    printf("%d\n", sum2);
-
-    free(arr);
-    free(timeSum);
     return 0;
 }
