f3e3e779470f88c7e5e37d028be46452d70a6706
changstudent <akscjfals@naver.com>
2023-01-21 09:14:17 +0900

    [problem 15873] A + B with no spaces

--- Challenge.c
+++ Challenge.c
@@ -1,65 +1,26 @@
 #include <stdio.h>
-#include <stdlib.h>
-int arrN[100000];
-int arrM[100000];
-int check[100000];
 
-// 이진탐색트리
-int binary_search_tree(int arr[], int key, int size)
-{
-    int front, rear, pivot;
-    front = 0;
-    rear = size - 1;
+int main(){
+    int input, backNumber = 0;
 
-    while (1)
-    {
-        pivot = (front + rear) / 2;
-        if (arr[pivot] == key)
-            return 1;
-        if (arr[front] == key)
-            return 1;
-        if (arr[rear] == key)
-            return 1;
+    scanf("%d", &input);
 
-        if (arr[pivot] < key)
-            front = pivot + 1;
-        else
-            rear = pivot - 1;
-
-        if (front >= rear)
-            return 0;
+    if (input == 1010)  // 10 + 10
+        printf("20\n");
+    
+    else if (input % 10 == 0) { // a + 10
+        input /= 100;
+        printf("%d", 10 + input);
     }
-}
+    
+    else {
+        backNumber += input % 10;
+        input /= 10;
 
-// 퀵 소트에서 사용할 비교함수
-int cmp(const void *a, const void *b)
-{
-    return *(int *)a > *(int *)b ? 1 : (*(int *)a < *(int *)b ? -1 : 0);
-}
-
-int main()
-{
-    int N, M;
-    int i;
-
-    scanf("%d", &N);
-    for (i = 0; i < N; i++)
-    {
-        scanf("%d", &arrN[i]);
-    }
-
-    scanf("%d", &M);
-    for (i = 0; i < M; i++)
-    {
-        scanf("%d", &arrM[i]);
-    }
-
-    // 퀵 소팅
-    qsort(arrN, N, sizeof(int), cmp);
-
-    for (i = 0; i < M; i++)
-    {
-        printf("%d\n", binary_search_tree(arrN, arrM[i], N));
+        if (input == 10) // 10 + backNumber
+            printf("%d\n", 10 + backNumber);
+        else    // input + backNumber
+            printf("%d\n", input + backNumber);
     }
 
     return 0;
