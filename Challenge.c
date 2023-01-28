f25fc65bf231bca4d5e35a0d90dc3065411e20d5
changstudent <akscjfals@naver.com>
2023-01-28 21:21:55 +0900

    [problem 1099] A star expression

--- Challenge.c
+++ Challenge.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-    int human, kill, counter = 0, index, add;
-    int* arr;
+    int layer;
 
-    scanf("%d %d", &human, &kill);
+    scanf("%d", &layer);
 
-    arr = (int*)calloc(human, sizeof(int));
-
-    for (int i = 0; i < human; i++) {
-        arr[i] = i + 1;
-    }
-
-    printf("<%d", arr[kill - 1]);
-    arr[kill - 1] = -1;
-
-    for (int i = 0, index = kill; i < human -1; i++) {
-        counter = 0;
-        add = -1;
-        while (counter < kill) {
-            add += 1;
-            if (arr[(index + add) % human] != -1)
-                counter += 1;
+    for (int i = 0; i < layer; i++) {
+        for (int j = 0; j < layer - i - 1; j++) {
+            printf(" ");
         }
-        index = (index + add) % human;
-        printf(", %d", arr[index]);
-        arr[index] = -1;
-        
+        for (int k = 0; k < i + 1; k++) {
+            printf("* ");
+        }
+        printf("\n");
     }
-    printf(">\n");
-
-    free(arr);
 
     return 0;
 }
