75784092405d6c61080e8a786bbf9555d72b1cdf
changstudent <akscjfals@naver.com>
2023-01-28 20:53:38 +0900

    [problem 11866] Josephus problem 0

--- Challenge.c
+++ Challenge.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
 int main() {
-    double average1, copyright;
-    int album, average;
+    int human, kill, counter = 0, index, add;
+    int* arr;
 
-    scanf("%d %d", &album, &average);
+    scanf("%d %d", &human, &kill);
 
-    average1 = average - 0.99;
+    arr = (int*)calloc(human, sizeof(int));
 
-    copyright = album * average1;
+    for (int i = 0; i < human; i++) {
+        arr[i] = i + 1;
+    }
 
-    printf("%0.lf", ceil(copyright));
+    printf("<%d", arr[kill - 1]);
+    arr[kill - 1] = -1;
+
+    for (int i = 0, index = kill; i < human -1; i++) {
+        counter = 0;
+        add = -1;
+        while (counter < kill) {
+            add += 1;
+            if (arr[(index + add) % human] != -1)
+                counter += 1;
+        }
+        index = (index + add) % human;
+        printf(", %d", arr[index]);
+        arr[index] = -1;
+        
+    }
+    printf(">\n");
+
+    free(arr);
 
     return 0;
 }
