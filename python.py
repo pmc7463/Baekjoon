b4ac22f1313b6a0e7c6c27f06054d77019851e6d
akscjfals@naver.com <akscjfals@naver.com>
2023-01-31 21:16:52 +0900

    [problem 1032] Command Prompt

--- python.py
+++ python.py
@@ -1,12 +1,12 @@
-list = []   # 리스트 선언
+N = int(input())                # 반복할 횟수 넣기
 
-for i in range(7):  # 7개의 숫자를 받는다.
-    num = int(input())
-    if num % 2 != 0:    # 받은 값이 짝수가 아니라면 list에 num을 추가한다.
-        list.append(num)    # num값을 list 뒷 부분에 계속 추가한다.
+firstStr = list(input())        # 문자열 리스트에 넣기 ['l', 'i', 's', 't']
+firstStr_len = len(firstStr)    # 첫번째 문자열 길이 구하기
 
-if len(list) == 0:  # 리스트의 길이가 0이면 -1을 출력한다.
-    print(-1)
-else:
-    print(sum(list))
-    print(min(list))
+for i in range(N - 1):          # 반복할 회수 만큼 반복
+    otherStr = list(input())    # 다른 문자열 넣기
+    for j in range(firstStr_len):   # 첫번째 문자열 길이 만큼 반복
+        if firstStr[j] != otherStr[j] :     # 첫번째 문자열과 다른 문자열의 비교하여 같지 않을 때
+            firstStr[j] = '?'   # 다르면 첫번째 문자열에 해당 인덱스에 '?' 넣어주기
+
+print(''.join(firstStr))        # '구분자'.join(리스트)함수 사용해서 문자를 문자열로 바꿔 주기,  ['l', 'i', 's', 't'] -> ['list']
