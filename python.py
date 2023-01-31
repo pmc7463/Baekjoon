0af0d07a7ba2a9714f4c0b81d083c301c71682af
akscjfals@naver.com <akscjfals@naver.com>
2023-01-31 21:46:40 +0900

    [problem 10815] Number Card

--- python.py
+++ python.py
@@ -1,12 +1,27 @@
-N = int(input())                # 반복할 횟수 넣기
+import sys  # sys.stdin.readline()을 쓰기위해서
+            # 반복문으로 여러 줄을 입력 받을 때 input()으로 받으면 시간초과가 발생 할 수 있다.
+n = int(input())
+card = list(map(int, sys.stdin.readline().split())) # map은 반복 가능한 객체(리스트 등)에 대한 각각의 요소들을 저장된 함수로 처리해주는 함수입니다.
 
-firstStr = list(input())        # 문자열 리스트에 넣기 ['l', 'i', 's', 't']
-firstStr_len = len(firstStr)    # 첫번째 문자열 길이 구하기
+m = int(input())
+check = list(map(int, sys.stdin.readline().split()))    # a,b,c에 대한 int 형 변환
 
-for i in range(N - 1):          # 반복할 회수 만큼 반복
-    otherStr = list(input())    # 다른 문자열 넣기
-    for j in range(firstStr_len):   # 첫번째 문자열 길이 만큼 반복
-        if firstStr[j] != otherStr[j] :     # 첫번째 문자열과 다른 문자열의 비교하여 같지 않을 때
-            firstStr[j] = '?'   # 다르면 첫번째 문자열에 해당 인덱스에 '?' 넣어주기
+card.sort()
 
-print(''.join(firstStr))        # '구분자'.join(리스트)함수 사용해서 문자를 문자열로 바꿔 주기,  ['l', 'i', 's', 't'] -> ['list']
+def binary_search(array, target, start, end):   # 이진 탐색 , 카드, 검사 카드, 0 ~ 카드 - 1
+    while start <= end:
+        mid = (start + end) // 2
+
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+for i in range(m):
+    if binary_search(card, check[i], 0, n - 1) is not None:
+        print(1, end = ' ')
+    else:
+        print(0, end = ' ')
