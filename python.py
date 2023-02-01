88fb5a1b57ad99b98d2dc4177cd7554e9330a056
akscjfals@naver.com <akscjfals@naver.com>
2023-02-01 16:14:25 +0900

    [problem 2693] The N th largest number

--- python.py
+++ python.py
@@ -1,27 +1,6 @@
-import sys  # sys.stdin.readline()을 쓰기위해서
-            # 반복문으로 여러 줄을 입력 받을 때 input()으로 받으면 시간초과가 발생 할 수 있다.
-n = int(input())
-card = list(map(int, sys.stdin.readline().split())) # map은 반복 가능한 객체(리스트 등)에 대한 각각의 요소들을 저장된 함수로 처리해주는 함수입니다.
+T = int(input())
 
-m = int(input())
-check = list(map(int, sys.stdin.readline().split()))    # a,b,c에 대한 int 형 변환
-
-card.sort()
-
-def binary_search(array, target, start, end):   # 이진 탐색 , 카드, 검사 카드, 0 ~ 카드 - 1
-    while start <= end:
-        mid = (start + end) // 2
-
-        if array[mid] == target:
-            return mid
-        elif array[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return None
-
-for i in range(m):
-    if binary_search(card, check[i], 0, n - 1) is not None:
-        print(1, end = ' ')
-    else:
-        print(0, end = ' ')
+for i in range(T):
+    array_A = list(map(int, input().split()))   # 문자열.split() 문자열을 구분자로 나눔
+    array_A.sort()
+    print(array_A[-3])
