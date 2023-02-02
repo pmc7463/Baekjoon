1e7cad275f4f4ada758e328d3d0affe9be5a87bb
akscjfals@naver.com <akscjfals@naver.com>
2023-02-02 19:35:15 +0900

    [problem 10886] 0 = not cute / 1 = cute

--- python.py
+++ python.py
@@ -1,6 +1,16 @@
-T = int(input())
-
-for i in range(T):
-    array_A = list(map(int, input().split()))   # 문자열.split() 문자열을 구분자로 나눔
-    array_A.sort()
-    print(array_A[-3])
+survey = int(input())
+cnt0 = 0
+cnt1 = 0
+if (survey % 2) == 0:
+    exit()
+else:
+    for i in range(survey):
+        vote = int(input())
+        if vote == 1:
+             cnt1 += 1
+        elif vote == 0:
+              cnt0 += 1
+if (cnt1 > cnt0):
+    print("Junhee is cute!")
+else:
+    print("Junhee is not cute!")
