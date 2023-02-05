3e4308d6e2dbbcd6123695271bd87af8d5bcf9a9
akscjfals@naver.com <akscjfals@naver.com>
2023-02-05 11:14:16 +0900

    [problem 2501] Divisor

--- python.py
+++ python.py
@@ -1,14 +1,13 @@
-testCase = int(input())
+arr = []
 
-for _ in range(testCase):
-    mars = list(map(str, input().split()))  # 문자열을 각 토근으로 쪼개서 받겠다.
-    answer = eval(mars[0])      # eval(expression)는 매개변수로 받은 expression(사칙연산 식)같은거을 문자열로 받어서 실행하는 함수이다.
-    for i in range(len(mars)):
-        if mars[i] == "@":
-            answer *= 3
-        elif mars[i] == "%":
-            answer += 5
-        elif mars[i] == "#":
-            answer -= 7
+N, K = map(int, input().split())
+cnt = 0
+for i in range(1, N + 1):   # 시작 부터 0으로 나눌 수 없으니까 1부터 시작해서 배열 끝 + 1까지
+    if N % i == 0:
+        arr.append(i)   # i값을 덧 붙여준다.
+    cnt += 1
 
-    print("%0.2f" %answer)  # %s는 문자열을 서식에 맞추어 출력해준다. (문자열만 해당됨)
+if K > len(arr):
+    print(0)
+else:
+    print(arr[K - 1])
