20a8528eddbf2db0f9c3f0c12a7940cb71850be2
akscjfals@naver.com <akscjfals@naver.com>
2023-02-06 17:51:16 +0900

    [problem 1676] Number of factory zeros

--- python.py
+++ python.py
@@ -1,6 +1,19 @@
-total = int(input())
+N = int(input())
 
-for _ in range(9):
-    total -= int(input())
+cnt = 0
 
-print(total)
+def factorial(N):   # def 사용자 정의 함수 user defined function
+    if N == 0:
+        return 1
+    else:
+        return N * factorial(N-1)
+
+fact = factorial(N)
+
+for i in range(len(str(fact))):
+    if fact % 10 == 0:   # %나머지 구할때 사용   
+        cnt += 1
+        fact //= 10     # //= 정수 나눗셈 (몫) 나눗셈 결과를 소수점을 버리고 정수만 얻고자 할때 사용
+ 
+print(cnt)
+
