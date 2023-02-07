09dd4d0700e42b6addab346b1560f9887d50c1d4
akscjfals@naver.com <akscjfals@naver.com>
2023-02-07 21:30:40 +0900

    [problem 1292] An easy problem

--- python.py
+++ python.py
@@ -1,19 +1,12 @@
-N = int(input())
+import sys
 
-cnt = 0
+a, b = map(int, sys.stdin.readline().split())
 
-def factorial(N):   # def 사용자 정의 함수 user defined function
-    if N == 0:
-        return 1
-    else:
-        return N * factorial(N-1)
+temp = []
 
-fact = factorial(N)
+for i in range(b + 1):  # 반복문을 통해 b구간까지의 수열을 만든다.
+    for j in range(i):
+        temp.append(i)  # temp리스트에 i값을 계속 붙여준다.
 
-for i in range(len(str(fact))):
-    if fact % 10 == 0:   # %나머지 구할때 사용   
-        cnt += 1
-        fact //= 10     # //= 정수 나눗셈 (몫) 나눗셈 결과를 소수점을 버리고 정수만 얻고자 할때 사용
- 
-print(cnt)
+print(sum(temp[a - 1 : b])) # a - 1 배열 부터 b 배열 까지 총 합 구하기 
 
