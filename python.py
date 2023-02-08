b1dd62403d2f42ddf5ee7c8fa8d705e606755ebc
akscjfals@naver.com <akscjfals@naver.com>
2023-02-08 17:07:09 +0900

    [problem 10992] A star expression

--- python.py
+++ python.py
@@ -1,12 +1,7 @@
-import sys
+star = int(input())
 
-a, b = map(int, sys.stdin.readline().split())
-
-temp = []
-
-for i in range(b + 1):  # 반복문을 통해 b구간까지의 수열을 만든다.
-    for j in range(i):
-        temp.append(i)  # temp리스트에 i값을 계속 붙여준다.
-
-print(sum(temp[a - 1 : b])) # a - 1 배열 부터 b 배열 까지 총 합 구하기 
-
+for i in range(1, star + 1):
+    if (i == 1 or i == star):
+        print(" " * (star - i) + "*" * (2*i-1))
+    else:
+        print(" " * (star - i) + "*" + " " * (2 * (i - 1) - 1) + "*")
