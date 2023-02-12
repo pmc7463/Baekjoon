520cb7e485dd6010e2011ffdb56c1223c362a980
akscjfals@naver.com <akscjfals@naver.com>
2023-02-12 19:44:25 +0900

    [prolem 2902] Abbreviation

--- python.py
+++ python.py
@@ -1,7 +1,3 @@
-number = []
-
-number = list(map(str, input(). split()))
-
-#print(number[0]+number[1])
-#print(number[2]+number[3])
-print(int(number[0]+number[1]) + int(number[2]+number[3]))
+a = list(input().split("-"))
+for i in a:
+    print(i[0], end = "")
