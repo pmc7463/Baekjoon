40ca0b05d0dce0e6bb29717ac71533f7e5bf5b9d
akscjfals@naver.com <akscjfals@naver.com>
2023-02-20 20:23:49 +0900

    [problem 2506] Score Calculation

--- python.py
+++ python.py
@@ -1,8 +1,16 @@
-woman, man, intern = map(int, input(). split())
-cnt = 0
-while woman >= 2 and man >= 1 and woman + man >= intern + 3: # 2명 , 1명 팀 만들 수 있고, 인턴쉽도 보낼 수 있는 수 일때
-    woman -= 2
-    man -= 1
-    cnt += 1
+problem = int(input())
 
-print(cnt)
+score = 0
+total = 0
+
+board = list(map(int, input().split()))
+
+for i in range(problem):
+    if board[i] == 1:
+        score += 1
+        total += score
+
+    else:
+        score = 0
+
+print(total)
