0d0cfaf7b3c2a456987d6f1be4052462af5645cd
changstudent <akscjfals@naver.com>
2023-02-21 17:16:49 +0900

    [problem 2460] Intelligent Train 2

--- python.py
+++ python.py
@@ -1,16 +1,14 @@
-problem = int(input())
+import sys
 
-score = 0
-total = 0
+input = sys.stdin.readline
 
-board = list(map(int, input().split()))
+result = 0 #최댓값
+num = 0 #사람들의 일시적인 값
 
-for i in range(problem):
-    if board[i] == 1:
-        score += 1
-        total += score
+for _ in range(10):
+    a, b = map(int, input().split())    #내린 사람, 탄 사람
+    num -= a    #내리다
+    num += b    #타다
+    result = max(result, num)   #최댓값 찾기
 
-    else:
-        score = 0
-
-print(total)
+print(result)
