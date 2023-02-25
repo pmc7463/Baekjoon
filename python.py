b63f72f029bfa2610477b93bfd1be7e8e2fe4f78
akscjfals@naver.com <akscjfals@naver.com>
2023-02-25 21:56:00 +0900

    [problem 15700] Fill Tiles 4

--- python.py
+++ python.py
@@ -1,47 +1,6 @@
 import sys
 input = sys.stdin.readline
 
-def merge_sort(L):
-    if len(L)  == 1:
-        return L
+N, M = map(int, input().split())
 
-    mid = (len(L) + 1) // 2
-
-    left = merge_sort(L[:mid])  #mid 전 까지 슬라이싱
-    right = merge_sort(L[mid:]) #mid이후 전부 슬라이싱
-
-    i, j = 0, 0
-    L2 = []
-
-    while i < len(left) and j < len(right):
-        if left[i] < right[j]:
-            L2.append(left[i])
-            ans.append(left[i])
-            i += 1
-
-        else:
-            L2.append(right[j])
-            ans.append(right[j])
-            j += 1
-
-    while i < len(left):
-        L2.append(left[i])
-        ans.append(left[i])
-        i += 1
-
-    while j < len(right):
-        L2.append(right[j])
-        ans.append(right[j])
-        j += 1
-
-    return L2
-
-n, k = map(int, input().split())
-a = list(map(int, input().split()))
-ans = []
-merge_sort(a)
-
-if len(ans) >= k:
-    print(ans[k - 1])
-else:
-    print(-1)
+print(N * M // 2)
