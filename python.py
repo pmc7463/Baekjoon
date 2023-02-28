8204a15165a645420f4692ce6bca22c4cb0d59a4
akscjfals@naver.com <akscjfals@naver.com>
2023-02-28 13:03:26 +0900

    [problem 4378] QWERTY

--- python.py
+++ python.py
@@ -1,8 +1,22 @@
-A = int(input())
-op = str(input())
-B = int(input())
+from sys import stdin
 
-if op == '+':
-    print(A+B)
-elif op == '*':
-    print(A*B)
+keyboard = "`1234567890-=QWERTYUIOP[]\\ASDFGHJKL;\'ZXCVBNM,./"
+
+while True:
+    sentence = stdin.readline().rstrip()
+    if sentence == '':
+        break
+
+    sentence_len = len(sentence)
+
+    result =''
+
+    for idx in range(sentence_len):
+        if sentence[idx] != ' ':
+            keyboard_idx = keyboard.index(sentence[idx])
+            result += keyboard[keyboard_idx - 1]
+
+        else:
+            result += ' '
+
+    print(result)
