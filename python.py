c0a91868114ed3657dc6343aadcb38e8ccc9f27c
akscjfals@naver.com <akscjfals@naver.com>
2023-03-03 19:47:07 +0900

    [problem 2476] A dice game

--- python.py
+++ python.py
@@ -1,5 +1,16 @@
 re = int(input())
+maxMoney = 0
 
-for _ in range(re):
-    a = str(input())
-    print(a.lower())
+for i in range(re):
+    a,b,c = map(int, input().split())
+    if a == b == c:
+        maxMoney = max(maxMoney, 10000+a*1000)
+    elif a == b:
+        maxMoney = max(maxMoney, 1000+a*100)
+    elif a == c:
+        maxMoney = max(maxMoney, 1000+a*100)
+    elif b == c:
+        maxMoney = max(maxMoney, 1000+b*100)
+    else:
+        maxMoney = max(maxMoney, max(a,b,c)*100)
+print(maxMoney)
