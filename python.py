27341056700cdf4c8f1ede0d51bc95d89ffc2601
akscjfals@naver.com <akscjfals@naver.com>
2023-03-04 16:35:06 +0900

    [problem 15894] Math is physical education

--- python.py
+++ python.py
@@ -1,16 +1,2 @@
-re = int(input())
-maxMoney = 0
-
-for i in range(re):
-    a,b,c = map(int, input().split())
-    if a == b == c:
-        maxMoney = max(maxMoney, 10000+a*1000)
-    elif a == b:
-        maxMoney = max(maxMoney, 1000+a*100)
-    elif a == c:
-        maxMoney = max(maxMoney, 1000+a*100)
-    elif b == c:
-        maxMoney = max(maxMoney, 1000+b*100)
-    else:
-        maxMoney = max(maxMoney, max(a,b,c)*100)
-print(maxMoney)
+floor = int(input())
+print(floor * 4)
