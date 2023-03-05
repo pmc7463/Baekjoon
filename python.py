9575b1bd6a2b30eddaf61e07ea253c251648fc6e
akscjfals@naver.com <akscjfals@naver.com>
2023-03-05 17:10:24 +0900

    [problem 5543] A hamburger set

--- python.py
+++ python.py
@@ -1,2 +1,12 @@
-floor = int(input())
-print(floor * 4)
+hamburger = []
+beverage = []
+
+for i in range(0,3):
+    a = int(input())
+    hamburger.append(a)
+
+for i in range(0,2):
+    b = int(input())
+    beverage.append(b)
+
+print(min(hamburger) + min(beverage) - 50)
