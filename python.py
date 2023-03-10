548cf77c358632aca1042a192cf8ecfeff158449
akscjfals@naver.com <akscjfals@naver.com>
2023-03-10 15:22:53 +0900

    [problem 9085] Plus

--- python.py
+++ python.py
@@ -1,16 +1,4 @@
-dwarf = []
-
-for i in range(9):
-    dwarf.append(int(input()))
-
-for i in range(9):
-    for j in range(i+1, 9):
-        if sum(dwarf) - dwarf[i] - dwarf[j] == 100:
-            A, B = i, j
-            break
-
-dwarf.pop(A)
-dwarf.pop(B-1)
-
-for i in dwarf:
-    print(i)
+for _ in range(int(input())):
+    re = int(input())
+    plus = list(map(int,input().split()))
+    print(sum(plus))
