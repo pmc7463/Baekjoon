e727bd348f20de3f1f276641f1c2e17bce76812d
akscjfals@naver.com <akscjfals@naver.com>
2023-03-12 14:53:36 +0900

    [problem 10820] String Analysis

--- python.py
+++ python.py
@@ -1,2 +1,22 @@
-A, B, C = map(int, input().split())
-print(A+B+C)
+import sys
+
+while True:
+    st = sys.stdin.readline().rstrip('\n')
+
+    if not st:
+        break
+
+    small, capital, num, blank = 0, 0, 0, 0
+
+    for i in range(len(st)):
+        if st[i].islower():
+            small += 1
+        elif st[i].isupper():
+            capital += 1
+        elif st[i].isdigit():
+            num += 1
+        else:
+            blank += 1
+
+    print(small, capital, num, blank)
+
