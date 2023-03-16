907155b02549ee85e39e552d23a181d0e3704c26
akscjfals@naver.com <akscjfals@naver.com>
2023-03-16 21:58:38 +0900

    [problem 10995] Star Expression -20

--- python.py
+++ python.py
@@ -1,13 +1,7 @@
 re = int(input())
-
-money = [25, 10, 5, 1]
-count = [0, 0, 0, 0]
+star = "*" + " *"*(re-1)
+space = False
 
 for i in range(re):
-    put = int(input())
-
-    for j in range(len(money)):
-        count[j] = put // money[j]
-        put %= money[j]
-
-    print(' '.join(map(str, count)))
+    print(" " + star if space else star)
+    space = not space
