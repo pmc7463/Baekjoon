9d2c23e6549a2d3596bf2fe2e2daec3091ac20e8
akscjfals@naver.com <akscjfals@naver.com>
2023-03-17 21:53:47 +0900

    [problem 3460] Binary number

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
 re = int(input())
-star = "*" + " *"*(re-1)
-space = False
 
-for i in range(re):
-    print(" " + star if space else star)
-    space = not space
+for _ in range(re):
+    method = bin(int(input()))[2:]  # 0b1101에서 0b를 제거해주기 위해서 사용
+    for i in range(len(method)):
+        if method[-i-1] == '1':     # method에 담긴 2진수를 역순으로 보기 위해서
+            print(i, end = ' ')
