4010b3d31ea6f2b6554f6fe377a7b5258ed7a8b8
akscjfals@naver.com <akscjfals@naver.com>
2023-03-18 14:13:31 +0900

    [problem 1026] Treasure

--- python.py
+++ python.py
@@ -1,7 +1,20 @@
 re = int(input())
 
-for _ in range(re):
-    method = bin(int(input()))[2:]  # 0b1101에서 0b를 제거해주기 위해서 사용
-    for i in range(len(method)):
-        if method[-i-1] == '1':     # method에 담긴 2진수를 역순으로 보기 위해서
-            print(i, end = ' ')
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+# A는 내림차순, B는 오름차순으로해서 곱해서 더해주면 답이 나오기는 하나 문제에서 B를 정렬을 못하게 하였다.
+#sorted_A = sorted(A, reverse=True)  #reverse = True 내림 차순 왼쪽에 큰숫자 오른쪽 작은 숫자
+#sorted_B = sorted(B)
+
+total = 0
+
+for i in range(re):
+    total += min(A) * max(B)    # 두 리스트에서 최솟값과 최댓값을 뽑아서 곱해서 더해준다.
+    A.pop(A.index(min(A)))      # A리스트에서 최솟값을 pop한다.
+    B.pop(B.index(max(B)))      # B리스트에서 최댓값을 pop한다.
+
+
+#print(sorted_A)
+#print(sorted_B)
+print(total)
