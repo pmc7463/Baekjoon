1fdc18fcf4d77126aa8a722abf129a1f38d2eecc
akscjfals@naver.com <akscjfals@naver.com>
2023-03-23 20:27:07 +0900

    [problem Boss, you have to gamble for fun.

--- python.py
+++ python.py
@@ -1,10 +1,11 @@
-N,M = map(int,input().split())
-arr = [0] * N #arr 0으로 초기화
+amount = 0
 
-for x in range(M):  #반복
-    i,j,k = map(int, input().split())
-    for y in range(i, j+1, 1):
-        arr[y-1] = k
+while True:
+    gambling = int(input())
 
-for x in range(N):
-    print(arr[x],'',end ='')    #한 줄로 출력
+    if gambling == -1:
+        break
+    else:
+        amount += gambling
+
+print(amount)
