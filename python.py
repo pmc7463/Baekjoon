4dde61611abef220d29a89d205ce64e92dd4221a
akscjfals@naver.com <akscjfals@naver.com>
2023-03-26 12:00:29 +0900

    [problem 1070] Water charge

--- python.py
+++ python.py
@@ -1,7 +1,21 @@
-re, k = map(int, input().split())
+x_charge = int(input()) #1리터당 요금
+y_basic = int(input())  #기본금액
 
-temp = list(map(int, input().split()))
+y_limit = int(input())  #기본요금 제한 금액
+y_additional = int(input()) #추가요금   1리터당 요금
+use = int(input())  #사용량 리터
 
-temp.sort()
+x_sum = x_charge * use
+y_sum = 0
 
-print(temp[k - 1])
+if use >= y_limit:
+    y_sum = use - y_limit
+    y_sum *= y_additional
+    y_sum += y_basic
+else:
+    y_sum = y_basic
+
+if x_sum <= y_sum:
+    print(x_sum)
+else:
+    print(y_sum)
