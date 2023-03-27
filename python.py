c9939a5104eeafa23b8ac37f0826658ec9c2ad74
akscjfals@naver.com <akscjfals@naver.com>
2023-03-27 20:13:25 +0900

    [problem 2745] Radix conversion

--- python.py
+++ python.py
@@ -1,21 +1,9 @@
-x_charge = int(input()) #1리터당 요금
-y_basic = int(input())  #기본금액
+N, b = input().split()
+ary = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-y_limit = int(input())  #기본요금 제한 금액
-y_additional = int(input()) #추가요금   1리터당 요금
-use = int(input())  #사용량 리터
+N = N[::-1]
+result = 0
 
-x_sum = x_charge * use
-y_sum = 0
-
-if use >= y_limit:
-    y_sum = use - y_limit
-    y_sum *= y_additional
-    y_sum += y_basic
-else:
-    y_sum = y_basic
-
-if x_sum <= y_sum:
-    print(x_sum)
-else:
-    print(y_sum)
+for i, n in enumerate(N):
+    result += (int(b)**i)*(ary.index(n))
+print(result)
