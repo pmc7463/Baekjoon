b2bf839bd7f1701d4acfe1ad5278a2541cd2b6f1
akscjfals@naver.com <akscjfals@naver.com>
2023-04-01 13:47:32 +0900

    [problem 9325] How much?

--- python.py
+++ python.py
@@ -1,11 +1,8 @@
-import math
-inch, h, w = map(int, input().split())  #대각선, 높이 비율, 너비 비율
-
-b = ((inch**2)/(((w/h)**2) + 1))**0.5   #피타고라스 비율 구하는 공식
-
-a = (b*w)/h
-
-#print(f"{b:.0f} {a:.0f}")
-#print(b, a)
-#print(round(b), round(a))
-print(math.floor(b), math.floor(a)) # math.floor = 소수점 이하 버리기는 함수
+re = int(input())
+for _ in range(re):
+    car = int(input())
+    option = int(input())
+    for _ in range(option):
+        amount, price = map(int, input().split())
+        car += amount * price
+    print(car)
