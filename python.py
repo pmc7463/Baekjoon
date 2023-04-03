24a781f5060ac7d6baf2ddda4da0c8364b329fa9
akscjfals@naver.com <akscjfals@naver.com>
2023-04-03 20:46:04 +0900

    [problem 15953] A prize hunter

--- python.py
+++ python.py
@@ -1,6 +1,17 @@
-S, A = map(int, input().split())
+import sys
+input = sys.stdin.readline
 
-if (S // 2) >= (A // 2):
-    print(A//2)
-else:
-    print(S//2)
+prevFes,fes = [0],[0]
+prevPrize,prize = (500,300,200,50,30,10),(512,256,128,64,32)
+for i in range(6):
+    prevFes += [prevPrize[i] for _ in range(i+1)]
+for i in range(5):
+    fes += [prize[i] for _ in range(2**i)]
+
+for _ in range(int(input())):
+    a,b = map(int,input().split())
+    if a >= len(prevFes):
+        a = 0
+    if b >= len(fes):
+        b = 0
+    print((prevFes[a] + fes[b])*10000)
