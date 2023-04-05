21927f0004dc5c53eedd63fb809f0c78a569a6cf
akscjfals@naver.com <akscjfals@naver.com>
2023-04-05 21:10:23 +0900

    [problem 1440] Time machine

--- python.py
+++ python.py
@@ -1,1 +1,7 @@
-print(ord(input()) - 44031)
+from itertools import permutations
+perms = list(permutations(map(int, input().split(":"))))
+count = 0
+for a, b, c in perms:
+    if 1 <= a <= 12 and 0 <= b <= 59 and 0 <= c <= 59:
+        count += 1
+print(count)
