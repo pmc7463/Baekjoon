1f043657d32a6ecc11c8c87456d9369801a27287
akscjfals@naver.com <akscjfals@naver.com>
2023-04-11 19:38:59 +0900

    [problem 1159] A basketball game

--- python.py
+++ python.py
@@ -1,17 +1,19 @@
-def fib(n):
-    if n==1 or n==2:
-        return 1
-    else:
-        return fib(n-1)+fib(n-2)
-        
-def fibonacci(n):
-    dp=[0]*(n+1)
-    dp[1],dp[2]=1,1
-    cnt2=0
-    for i in range(3,n+1):
-        cnt2+=1
-        dp[i]=dp[i-1]+dp[i-2]
-    return cnt2
-
-n=int(input())
-print(fib(n),fibonacci(n))
+from collections import Counter
+n = int(input())
+player = []
+fn = []
+cnt = 0
+for i in range(n):
+    a = input()
+    player.append(a[0])
+player_count = Counter(player)
+for i, j in player_count.items():
+    if j >= 5:
+        fn.append(i)
+        cnt += 1
+fn.sort()
+if cnt == 0:
+    print("PREDAJA")
+else:
+    for i in fn:
+        print(i, end='')
