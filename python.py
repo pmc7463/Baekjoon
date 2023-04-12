012e87ac4c13015eac8c1f04358d23a309f30a88
akscjfals@naver.com <akscjfals@naver.com>
2023-04-12 20:58:21 +0900

    [problem 10569] Polyhedron

--- python.py
+++ python.py
@@ -1,19 +1,6 @@
-from collections import Counter
-n = int(input())
-player = []
-fn = []
-cnt = 0
-for i in range(n):
-    a = input()
-    player.append(a[0])
-player_count = Counter(player)
-for i, j in player_count.items():
-    if j >= 5:
-        fn.append(i)
-        cnt += 1
-fn.sort()
-if cnt == 0:
-    print("PREDAJA")
-else:
-    for i in fn:
-        print(i, end='')
+N = int(input())
+
+for i in range(N):
+    V, E = map(int, input().split())
+    print(abs(V-E-2))
+
