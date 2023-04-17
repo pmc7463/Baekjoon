b4109f351e8efcc82341dbb3b38d6cee2b9bf758
akscjfals@naver.com <akscjfals@naver.com>
2023-04-17 19:27:12 +0900

    [problem 3034] Putting in a match

--- python.py
+++ python.py
@@ -1,2 +1,8 @@
-str1 = input()
-print(str1.upper())
+N, W, H = map(int, input().split())
+
+for _ in range(N):
+    check = int(input())
+    if check <= W or check <= H or (check ** 2 <= (W ** 2 + H ** 2)):
+        print("DA")
+    else:
+        print("NE")
