b39e32f3b6ae064679f05af3a1db8f8f093c8186
akscjfals@naver.com <akscjfals@naver.com>
2023-04-22 16:25:15 +0900

    [problem 13752] Histogram

--- python.py
+++ python.py
@@ -1,11 +1,5 @@
-count = [0] * (1000 + 1) # 개수를 셀 0 ~ 1000까지의 리스트를 0으로 초기화
+re = int(input())
 
-Sum = 0
-
-for _ in range(10):
-    num = int(input())
-    Sum += num
-    count[num] += 1 # 해당 num의 개수를 +1 씩 해준다.
-
-print(Sum // 10)
-print(count.index(max(count)))
+for i in range(re):
+    gram = int(input())
+    print("="*gram)
