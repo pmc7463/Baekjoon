8694f2c120d0880536c7592ca3bdf9b23edeb7a4
akscjfals@naver.com <akscjfals@naver.com>
2023-04-23 20:59:01 +0900

    [problem 1453] Part-time jop at an internet cafe

--- python.py
+++ python.py
@@ -1,5 +1,7 @@
-re = int(input())
+repetition = int(input())
 
-for i in range(re):
-    gram = int(input())
-    print("="*gram)
+chair = list(map(int, input().split())) # [1, 2, 3]
+
+overlap = len(list(set(chair))) # set함수는 중복 된 값들을 제거하고 남은 고유의 값 개수, len은 전체 길이
+
+print(repetition-overlap)
