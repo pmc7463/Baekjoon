c3b1ee8dcdb950a03bfb6e9f1f33d9a83adcda8f
akscjfals@naver.com <akscjfals@naver.com>
2023-04-29 14:42:29 +0900

    [problem 4504] Find multiple

--- python.py
+++ python.py
@@ -1,6 +1,11 @@
-repetition = int(input())
+mul = int(input())
 
-for i in range(1, repetition + 1):
-    dice1, dice2 = map(int, input().split())
-    if dice1 >= 1 and dice1 <=  6 and dice2 >= 1 and dice2 <=  6:
-        print(f"Case {i}: {dice1 + dice2}")
+while True:
+    check = int(input())
+    if check != 0:
+        if check % mul == 0:
+            print(f"{check} is a multiple of {mul}.")
+        else:
+            print(f"{check} is NOT a multiple of {mul}.")
+    else:
+        break
