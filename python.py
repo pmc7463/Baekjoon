ff0896466a913adfdecb3180e46e86b32c4ec975
akscjfals@naver.com <akscjfals@naver.com>
2023-04-30 22:43:26 +0900

    [problem 2903] Central Move Algorithm

--- python.py
+++ python.py
@@ -1,11 +1,1 @@
-mul = int(input())
-
-while True:
-    check = int(input())
-    if check != 0:
-        if check % mul == 0:
-            print(f"{check} is a multiple of {mul}.")
-        else:
-            print(f"{check} is NOT a multiple of {mul}.")
-    else:
-        break
+print((2**int(input())+1)**2)
