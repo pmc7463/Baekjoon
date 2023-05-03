5fa5888ad541c14d4e509f918e1d2339924746d1
akscjfals@naver.com <akscjfals@naver.com>
2023-05-03 15:25:21 +0900

    [problem 10811] Turning over a basket

--- python.py
+++ python.py
@@ -1,5 +1,11 @@
-re = int(input())
+number, number_of_times = map(int, input().split())
 
-for i in range(re):
-    P, C = map(int, input().split())
-    print(f"You get {P//C} piece(s) and your dad gets {P%C} piece(s).")
+arr = []
+
+for i in range(1, number + 1):
+    arr.append(i)
+
+for i in range(number_of_times):
+    a, b = map(int, input().split())
+    arr[a-1:b] = reversed(arr[a-1:b])   # reversed함수는 인자로 전달된 시퀀스 자료형을 뒤집어서 반환하는 함수
+print(' '.join(map(str, arr)))  # [3, 4, 1, 2, 5] --> 3 4 1 2 5
