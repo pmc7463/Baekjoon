0f2dfbf2385b062fcf9b35adef6410e46ececa70
akscjfals@naver.com <akscjfals@naver.com>
2023-05-05 19:27:40 +0900

    [problem 9506] The sum of the mimeral water

--- python.py
+++ python.py
@@ -1,15 +1,19 @@
 while True:
-    num = list(map(int, input()))
-    count = 0
-    if num[0] == 0:
+    num = int(input())
+    if num == -1:
         break
     else:
-        for i in range(len(num)):
-            if num[i] == 0:
-                count += 4
-            elif num[i] == 1:
-                count += 2
-            else:
-                count += 3
+        rest = []
+        for i in range(1, num+1):
+            if num % i == 0:
+                rest.append(i)
 
-        print(count + len(num) + 1)
+        sumTemp = sum(rest[:-1])
+
+        if sumTemp == num:
+            print(f"{num} = {' + '.join(map(str, rest[:-1]))}")     
+            #join 메서드는 이용해서 문자열을 구분자로 이용하여 리스트의 모든 요소를 연결해주는 메서드 입니다.
+            #map 함수를 이용하여 리스트의 모든 요소를 문자열로 반환한 후 join 메서드를 사용하면 간단하게 리스트 안에 있는 모든 숫자를 하나의 문자열로 연결할 수 잇습니다.
+            #6 = 1 + 2 + 3 한 줄로 출력
+        else:
+            print(f"{num} is NOT perfect.")
