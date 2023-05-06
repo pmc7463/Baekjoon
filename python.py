6196b9862a32b286338a634fc9a797634cc7a209
akscjfals@naver.com <akscjfals@naver.com>
2023-05-06 18:37:51 +0900

    [problem 10773] Zero

--- python.py
+++ python.py
@@ -1,24 +1,10 @@
-counter = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]  # 초기화
-
-
-def school(sex, year):
-    if sex == 0:
-        counter[0][year - 1] += 1
+repetition = int(input())
+arr = []
+for _ in range(repetition):
+    num = int(input())
+    if num == 0:
+        arr.pop(len(arr) - 1)
     else:
-        counter[1][year - 1] += 1
+        arr.append(num)
 
-
-student, room = map(int, input().split())
-
-for i in range(student):
-    sex, year = map(int, input().split())
-    school(sex, year)
-
-room_count = 0
-for i in range(6):
-    for j in range(2):
-        room_count += counter[j][i] // room
-        if counter[j][i] % room > 0:
-            room_count += 1
-
-print(room_count)
+print(sum(arr))
