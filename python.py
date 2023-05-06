e3e35cb08b71a16bf99fe0a057b7f2f280fabda6
akscjfals@naver.com <akscjfals@naver.com>
2023-05-06 18:17:02 +0900

    [problem 13300] Room assignment

--- python.py
+++ python.py
@@ -1,8 +1,24 @@
-domino = int(input())
-sum = 0
+counter = [[0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]  # 초기화
 
-for i in range(domino+1):
-    for j in range(i, domino+1):
-        sum += i + j
 
-print(sum)
+def school(sex, year):
+    if sex == 0:
+        counter[0][year - 1] += 1
+    else:
+        counter[1][year - 1] += 1
+
+
+student, room = map(int, input().split())
+
+for i in range(student):
+    sex, year = map(int, input().split())
+    school(sex, year)
+
+room_count = 0
+for i in range(6):
+    for j in range(2):
+        room_count += counter[j][i] // room
+        if counter[j][i] % room > 0:
+            room_count += 1
+
+print(room_count)
