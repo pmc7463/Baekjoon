cfa8ec2c4e88f76839b3066a43ac8edaad1c005d
akscjfals@naver.com <akscjfals@naver.com>
2023-05-08 22:26:25 +0900

    [problem 25314] Coding is a physical education course

--- python.py
+++ python.py
@@ -1,8 +1,7 @@
-password = input()
+dataType = int(input())
 
-for i in password:
-    if ord(i) < 68: #ord('A') --> 65 
-        print(chr(ord(i) + 23), end = "")   #char(65) --> A
-
-    else:
-        print(chr(ord(i) - 3), end = "")
+if dataType == 4:
+    print("long int")
+else:
+    temp = dataType // 4
+    print("long "*temp, end = "int")
