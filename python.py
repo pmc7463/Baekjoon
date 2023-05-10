626cd5300d1273a83c2ca7152859a47967a81c1d
akscjfals@naver.com <akscjfals@naver.com>
2023-05-10 17:35:40 +0900

    [problem 3004] A price of chessboard

--- python.py
+++ python.py
@@ -1,12 +1,6 @@
-finish_Day, grow_Day, room, price = map(int, input().split())
+saw = int(input())
 
-i = 0
-count = 0
-while True:
-    if i * grow_Day + 1 <= finish_Day:
-        count += 1
-        i += 1
-    else:
-        break
+row = saw // 2
+col = saw - row
 
-print((count - 1) * room * price)
+print((row+1)*(col+1))
