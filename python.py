84c9141376e0b84ac8b958dec7d4648d35d0121d
akscjfals@naver.com <akscjfals@naver.com>
2023-05-14 20:33:41 +0900

    [problem 24263] Running time of the algorithm 2

--- python.py
+++ python.py
@@ -1,11 +1,2 @@
-t = int(input())
-stack = []
-
-for _ in range(t):
-    bar = int(input())
-
-    while stack and stack[-1] <= bar:
-        stack.pop()
-    stack.append(bar)
-
-print(len(stack))
+print(input())
+print(1)
