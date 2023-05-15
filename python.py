efce084e7cfd9f39f2e1eb50587aebbe72cd9f85
akscjfals@naver.com <akscjfals@naver.com>
2023-05-15 22:09:18 +0900

    [problem 2789] A ban on studying abroad

--- python.py
+++ python.py
@@ -1,2 +1,9 @@
-print(input())
-print(1)
+a = "CAMBRIDGE"
+s = list(input())
+for i in a:
+    for j in range(len(s)):
+        if i == s[j]:
+            s[j] = ''
+for i in s:
+    print(i, end='')
+
