64157025cc62443b9a45174f15b8ab1f03ad7e1f
akscjfals@naver.com <akscjfals@naver.com>
2023-05-21 17:53:27 +0900

    [problem 12790] Mini Fantasy War

--- python.py
+++ python.py
@@ -1,13 +1,20 @@
-Re = int(input())
-for _ in range(Re):
-    School = int(input())
-    Alcohol_max = 0
-    Final_name = ""
-    for _ in range(School):
-        School_name, School_alcohol = input().split()
-        School_alcohol = int(School_alcohol)
-        if Alcohol_max < School_alcohol:
-            Alcohol_max = School_alcohol
-            Final_name = School_name
+character = int(input())
 
-    print(Final_name)
+for _ in range(character):
+    HP, MP, Offensive, Defensive, HP_a, MP_a, Offensive_a, Defensive_a = map(int, input().split())
+
+    fighting1 = HP + HP_a
+    fighting2 = MP + MP_a
+    fighting3 = Offensive + Offensive_a
+    fighting4 = Defensive + Defensive_a
+
+    if HP + HP_a < 1:
+        fighting1 = 1
+
+    if MP + MP_a < 1:
+        fighting2 = 1
+
+    if Offensive + Offensive_a < 0:
+        fighting3 = 0
+
+    print(fighting1 + 5 * fighting2 + 2 * fighting3 + 2* fighting4)
