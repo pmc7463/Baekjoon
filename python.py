c4ffed4f35528c92d21cbf1ba44a8ebf158a3754
akscjfals@naver.com <akscjfals@naver.com>
2023-05-22 21:45:47 +0900

    [problem 10821] The number of integers

--- python.py
+++ python.py
@@ -1,20 +1,8 @@
-character = int(input())
+str = input().split(",")
 
-for _ in range(character):
-    HP, MP, Offensive, Defensive, HP_a, MP_a, Offensive_a, Defensive_a = map(int, input().split())
+count = 0
 
-    fighting1 = HP + HP_a
-    fighting2 = MP + MP_a
-    fighting3 = Offensive + Offensive_a
-    fighting4 = Defensive + Defensive_a
+for i in str:
+    count += 1
 
-    if HP + HP_a < 1:
-        fighting1 = 1
-
-    if MP + MP_a < 1:
-        fighting2 = 1
-
-    if Offensive + Offensive_a < 0:
-        fighting3 = 0
-
-    print(fighting1 + 5 * fighting2 + 2 * fighting3 + 2* fighting4)
+print(count)
