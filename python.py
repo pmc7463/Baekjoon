1a2fb0da7c5901c92f9a6af8c5834d011579fc81
akscjfals@naver.com <akscjfals@naver.com>
2023-05-23 20:28:16 +0900

    [problem 14215] Three stick

--- python.py
+++ python.py
@@ -1,8 +1,15 @@
-str = input().split(",")
+input_str = input() # "1 2 3"과 같은 입력을 받음
+numbers = input_str.split() # 공백으로 숫자들을 분리하여 리스트로 저장
+numbers = [int(num) for num in numbers] # 문자열을 정수로 변환하여 리스트에 저장
 
-count = 0
+sort = sorted(numbers)
 
-for i in str:
-    count += 1
+Sum = int(sum(sort[:2]))
 
-print(count)
+Max_list = sort[2:3]
+Max = int(Max_list[0])
+
+if Sum > Max:
+    print(sum(numbers))
+else:
+    print(Sum*2-1)
