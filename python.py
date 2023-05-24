dd0e8e8b8673493f89e91ffe3ecfffe8f81562c1
akscjfals@naver.com <akscjfals@naver.com>
2023-05-24 19:22:21 +0900

    [problem 5361] Battle droid price

--- python.py
+++ python.py
@@ -1,15 +1,16 @@
-input_str = input() # "1 2 3"과 같은 입력을 받음
-numbers = input_str.split() # 공백으로 숫자들을 분리하여 리스트로 저장
-numbers = [int(num) for num in numbers] # 문자열을 정수로 변환하여 리스트에 저장
+case = int(input())
 
-sort = sorted(numbers)
+for i in range(case):
+    part = input()  # "1 2 3" 과 같은 입력을 받음
+    numbers = part.split()  # 공백으로 숫자들을 분리하여 리스트로 저장
+    numbers = [int(num) for num in numbers] # 문자열을 정수로 변환하여 리스트에 저장
 
-Sum = int(sum(sort[:2]))
+    money = [350.34,230.90,190.55,125.30,180.90]
 
-Max_list = sort[2:3]
-Max = int(Max_list[0])
+    sum = 0
 
-if Sum > Max:
-    print(sum(numbers))
-else:
-    print(Sum*2-1)
+    for i in range(5):
+        sum += numbers[i] * money[i]
+
+    formatted_sum = '{:.2f}'.format(sum)
+    print(f"${formatted_sum}")
