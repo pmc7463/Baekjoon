e065aab93137a2d9b0a4d212d91a4a3d057c97c9
akscjfals@naver.com <akscjfals@naver.com>
2023-05-25 21:31:35 +0900

    [problem 9550] Kids love candy

--- python.py
+++ python.py
@@ -1,16 +1,14 @@
-case = int(input())
+re = int(input())
 
-for i in range(case):
-    part = input()  # "1 2 3" 과 같은 입력을 받음
-    numbers = part.split()  # 공백으로 숫자들을 분리하여 리스트로 저장
-    numbers = [int(num) for num in numbers] # 문자열을 정수로 변환하여 리스트에 저장
+for _ in range(re):
+    case, eat = map(int, input().split())
 
-    money = [350.34,230.90,190.55,125.30,180.90]
+    candy_Sum = 0
 
-    sum = 0
+    candy = input()
+    candy_list = list(map(int, candy.split()))
 
-    for i in range(5):
-        sum += numbers[i] * money[i]
+    for i in range(case):
+        candy_Sum += candy_list[i] // eat
 
-    formatted_sum = '{:.2f}'.format(sum)
-    print(f"${formatted_sum}")
+    print(candy_Sum)
