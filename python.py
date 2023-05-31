7b630718f34a2581b889409b9904f12c5bf0cd03
akscjfals@naver.com <akscjfals@naver.com>
2023-05-31 17:48:17 +0900

    [problem 10867] To sort without duplication

--- python.py
+++ python.py
@@ -1,17 +1,5 @@
-import sys
+re = int(input())
 
-num1 = int(sys.stdin.readline())
+temp = list(set(list(map(int, input().split()))))
 
-num = sys.stdin.readline().rstrip() #개행 문자 제거
-num_list = list(map(int, num.split()))
-
-prefix_sum = [0] * (num1 + 1)
-
-for i in range(1, num1 + 1):
-    prefix_sum[i] = prefix_sum[i-1] + num_list[i - 1]
-
-num2 = int(sys.stdin.readline())
-
-for i in range(num2):
-    A, B = map(int, sys.stdin.readline().split())
-    print(prefix_sum[B] - prefix_sum[A-1])
+print(*sorted(temp))
