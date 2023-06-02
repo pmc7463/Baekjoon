77ae45a7d012167d03d9b706897b9bd3bb24fc04
akscjfals@naver.com <akscjfals@naver.com>
2023-06-02 18:02:41 +0900

    [prooblem 1598] List the numbers that bite the tail

--- python.py
+++ python.py
@@ -1,13 +1,4 @@
-while True:
-    num = input()
-    num_list = list(map(int, num.split()))
-
-    if num_list[0] == -1:
-        break
-    else:
-        count = 0
-        for i in range(len(num_list) - 1):
-            for j in range(len(num_list) - 1):
-                if num_list[i] * 2 == num_list[j]:
-                    count += 1
-        print(count)
+a, b = map(int, input().split())
+a -= 1
+b -= 1
+print(abs(a // 4 - b // 4) + abs(a % 4 - b % 4))
