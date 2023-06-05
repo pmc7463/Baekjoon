145ef09dcfb1c957dc3f43ae8175639202bdb543
akscjfals@naver.com <akscjfals@naver.com>
2023-06-05 22:04:01 +0900

    [problem 17863] FYI

--- python.py
+++ python.py
@@ -1,23 +1,6 @@
-re = int(input())
+num = input()
 
-array = []
-
-for _ in range (re):
-    num = input()   # 한 줄 입력 받음
-    values = num.split()
-    array.extend(values)
-
-array = list(map(int, array))
-
-odd = []
-even = []
-for i in range(len(array)):
-    if i % 2 != 0:
-        odd.append(array[i])
-    else:
-        even.append(array[i])
-
-x = max(odd) - min(odd)
-y = max(even) - min(even)
-
-print(x * y)
+if num[:3] == '555':
+    print("YES")
+else:
+    print("NO")
