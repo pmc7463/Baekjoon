5c265ac98b7e14aae59a8d502b010855fe0d5e17
akscjfals@naver.com <akscjfals@naver.com>
2023-06-07 22:17:45 +0900

    [problem 3059] The sum of characters that do not apper

--- python.py
+++ python.py
@@ -1,10 +1,16 @@
 re = int(input())
 
 for _ in range(re):
-    str = input()
-    num = int(str.split()[0])
-    name = list(str.split()[1])
-    name.pop(num-1)
-    for i in name:
-        print(i, end='')
-    print()
+    input_str = input()
+    str_list = list(input_str)
+
+    uppercase_set = set()  # 등장한 대문자들을 저장하기 위한 집합
+    total_possible_sum = (ord('A') + ord('Z')) * (ord('Z') - ord('A') + 1) // 2
+
+    for char in str_list:
+        if 'A' <= char <= 'Z':  # 대문자인 경우에만 처리
+            uppercase_set.add(char)
+
+    missing_uppercase_sum = total_possible_sum - sum(ord(char) for char in uppercase_set)
+
+    print(missing_uppercase_sum)
