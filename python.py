472c410ee0f638253e600461b26b84be3a80edbf
akscjfals@naver.com <akscjfals@naver.com>
2023-06-10 22:47:47 +0900

    [problem 24265] Big-O_problem 4

--- python.py
+++ python.py
@@ -1,10 +1,3 @@
-input_str = input()
-x = list(input_str.split()[0])
-y = list(input_str.split()[1])
-
-x = ''.join(x)
-y = ''.join(y)
-
-Finish_reversed = str(int(x[::-1]) + int(y[::-1]))
-
-print(int(Finish_reversed[::-1]))
+n = int(input())
+print(int(n*(n-1)/2))
+print(2)
