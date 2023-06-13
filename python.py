36d1d29832a503f213a635f92225f4d7c03e7035
akscjfals@naver.com <akscjfals@naver.com>
2023-06-13 20:46:27 +0900

    [problem 2740] Matrix multiplication

--- python.py
+++ python.py
@@ -1,9 +1,16 @@
-stick = int(input())
-cnt = 0
+A_x, A_y = map(int, input().split())
+A = [list(map(int, input().split())) for _ in range(A_x)]
 
-while stick != 0:
-    if stick % 2 == 1:
-        cnt += 1
-    stick = stick // 2
+B_x, B_y = map(int, input().split())
+B = [list(map(int, input().split())) for _ in range(B_x)]
 
-print(cnt)
+AxB = [[0] * B_y for _ in range(A_x)]
+for row in range(A_x):
+    for col in range(B_y):
+        e = 0
+        for i in range(A_y):
+            e += A[row][i] * B[i][col]
+        AxB[row][col] = e
+
+for r in AxB:
+    print(*r)
