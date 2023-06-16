a783757195c0a3f0545c9ea393564c518c0c5640
Min Fe 97 <77577896+pmc7463@users.noreply.github.com>
2023-06-16 22:45:49 +0900

    [problem 2851] Super mario

--- python.py
+++ python.py
@@ -1,9 +1,11 @@
-A = list(input())
-B = list(input())
-
-A1, B1 = A[:], B[:]
-
-remove_A = [x for x in A if not x in B1 or B1.remove(x)]
-remove_B = [x for x in B if not x in A1 or A1.remove(x)]
-
-print(len(remove_A) + len(remove_B))
+m = []
+score = 0
+for i in range(10):
+    m.append(int(input()))
+for j in m:
+    score += j
+    if score >= 100:
+        if score - 100 > 100 - (score - j):
+            score -= j
+        break
+print(score)
