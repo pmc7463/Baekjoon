aa133184a6b5d79b53c71a56d9ae94ce1608c504
changstudent <akscjfals@naver.com>
2023-06-18 22:45:20 +0900

    [problem 10448] Eureka theory

--- python.py
+++ python.py
@@ -1,12 +1,12 @@
+triangle = [n*(n+1)//2 for n in range(1, 46)]
+eureka = [0] * 1001
 
-n = int(input())
+for i in triangle:
+    for j in triangle:
+        for k in triangle:
+            if i+j+k <= 1000:
+                eureka[i+j+k] = 1
 
-milk_list = list(map(int, input().split()))
-
-count = 0
-
-for i in range(n):
-    if milk_list[i] == count % 3:
-        count += 1
-
-print(count)
+T = int(input())
+for _ in range(T):
+    print(eureka[int(input())])
