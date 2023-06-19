2f24885ba0a4f27b2cfafad1d1091a3cb1da3ad2
akscjfals@naver.com <akscjfals@naver.com>
2023-06-19 21:46:16 +0900

    [problem 14696] A game of slap-match

--- python.py
+++ python.py
@@ -1,12 +1,24 @@
-triangle = [n*(n+1)//2 for n in range(1, 46)]
-eureka = [0] * 1001
-
-for i in triangle:
-    for j in triangle:
-        for k in triangle:
-            if i+j+k <= 1000:
-                eureka[i+j+k] = 1
-
-T = int(input())
-for _ in range(T):
-    print(eureka[int(input())])
+round = int(input())
+for i in range(round) :
+    A = list(map(int,input().split()))[1:]
+    B = list(map(int,input().split()))[1:]
+    lstA = [A.count(4),A.count(3),A.count(2),A.count(1)]
+    lstB = [B.count(4),B.count(3),B.count(2),B.count(1)]
+    if lstA[0] > lstB[0] :
+        print('A')
+    elif lstA[0] <lstB[0] :
+        print('B')
+    elif lstA[1] > lstB[1] :
+        print('A')
+    elif lstB[1] > lstA[1] :
+        print('B')
+    elif lstA[2] > lstB[2] :
+        print('A')
+    elif lstB[2] > lstA[2] :
+        print('B')
+    elif lstA[3] > lstB[3] :
+        print('A')
+    elif lstB[3] > lstA[3] :
+        print('B')
+    else :
+        print('D')
