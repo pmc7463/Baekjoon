5a8789e6e9b1bd9751f7776c8573f73835d04128
akscjfals@naver.com <akscjfals@naver.com>
2023-06-22 18:27:46 +0900

    [problem 10179] Coupon

--- python.py
+++ python.py
@@ -1,4 +1,8 @@
-N = input()
-A = list(map(int, input().split()))
-A.sort()
-print(A[-1])
+repetition = int(input())
+
+for _ in range(repetition):
+    price = float(input())
+
+    discount = price - price * 0.2
+
+    print(f"${discount:.2f}")
