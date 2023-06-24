c5c02f785cb79d744d11ad5ea60c657620096cbf
akscjfals@naver.com <akscjfals@naver.com>
2023-06-24 21:41:43 +0900

    [problem 2783] Triangular rolled-rice gimbap

--- python.py
+++ python.py
@@ -1,8 +1,15 @@
+A_price, A_gram = map(int, input().split())
+
 repetition = int(input())
 
+A_store = (1000 / A_gram) * A_price
+
+min_price = A_store
+
 for _ in range(repetition):
-    price = float(input())
+    x_price, x_gram = map(int, input().split())
+    amount = (1000 / x_gram) * x_price
+    if min_price > amount:
+        min_price = amount
 
-    discount = price - price * 0.2
-
-    print(f"${discount:.2f}")
+print(f"{min_price:.2f}")
