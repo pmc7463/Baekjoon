95a9bc6a7b2a6c83d792b1a8a55bee48152049cd
akscjfals@naver.com <akscjfals@naver.com>
2023-07-13 19:52:41 +0900

    [probelm 1333] Unanswered call

--- python.py
+++ python.py
@@ -1,2 +1,18 @@
-a, b = map(int, input().split())
-print(int(a*(100-b)/100<100))
+import sys
+
+N, L, D = map(int, sys.stdin.readline().split())
+
+Time = [False] * (N*L+(N-1)*5)
+
+for i in range(0, N*L+(N-1)*5, L+5):
+    for j in range(i, i+L):
+        Time[j] = True
+
+for i in range(0, N*L+(N-1)*5, D):
+    if not Time[i]:
+        print(i)
+        break
+else:
+    print(i+D)
+
+
