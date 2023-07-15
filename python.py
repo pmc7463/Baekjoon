ea240009460bbb3347adbbf2529c91b6b2cf11c4
akscjfals@naver.com <akscjfals@naver.com>
2023-07-15 20:44:20 +0900

    [problem 2161] Card 1

--- python.py
+++ python.py
@@ -1,38 +1,20 @@
-from collections import deque
+import sys
+input = sys.stdin.readline()
 
-N, M = map(int, input().split())
+num = int(input)
 
-graph = []
+arr = []
 
-for _ in range(N):
-    graph.append(list(map(int, input())))
+for i in range(1, num + 1):
+    arr.append(i)
 
-def bfs(x, y):
+temp = []
 
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+while len(arr) != 0:
+    temp.append(arr.pop(0))
 
-    # deque 생성
-    queue = deque()
-    queue.append((x, y))
+    if len(arr) != 0:
+        arr.append(arr.pop(0))
 
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-
-            if nx < 0 or nx >= N or ny < 0 or ny >= M:
-                continue
-
-            if graph[nx][ny] == 0:
-                continue
-
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx, ny))
-
-    return graph[N - 1][M - 1]
-
-print(bfs(0, 0))
+for i in temp:
+    print(i, end = " ")
