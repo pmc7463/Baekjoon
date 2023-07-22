fcd759233d2bf82d1b48acceb8d31df34ef7205f
akscjfals@naver.com <akscjfals@naver.com>
2023-07-22 18:20:29 +0900

    [problem 4949] A balanced world

--- python.py
+++ python.py
@@ -1,40 +1,29 @@
-import sys
+while True:
+    sentence = input()
+    stack = []
 
-repetition = int(sys.stdin.readline())
+    if sentence == ".":
+        break
 
-for _ in range(repetition):
-    win = int(sys.stdin.readline())
-    P1_cnt = 0
-    P2_cnt = 0
-    for _ in range(win):
-        P1, P2 = map(str, input().split())
-        if P1 == "R" and P2 == "S":
-            P1_cnt += 1
-        elif P1 == "R" and P2 == "P":
-            P2_cnt += 1
-        elif P1 == "R" and P2 == "R":
-            P1_cnt += 1
-            P2_cnt += 1
+    for i in sentence:
+        if i == "[" or i == "(":
+            stack.append(i)
 
-        elif P1 == "S" and P2 == "R":
-            P2_cnt += 1
-        elif P1 == "S" and P2 == "P":
-            P1_cnt += 1
-        elif P1 == "S" and P2 == "S":
-            P1_cnt += 1
-            P2_cnt += 1
+        elif i == "]":
+            if len(stack) != 0 and stack[-1] == "[":
+                stack.pop()
+            else:
+                stack.append("]")
+                break
 
-        elif P1 == "P" and P2 == "R":
-            P1_cnt += 1
-        elif P1 == "P" and P2 == "S":
-            P2_cnt += 1
-        elif P1 == "P" and P2 == "P":
-            P1_cnt += 1
-            P2_cnt += 1
+        elif i == ")":
+            if len(stack) != 0 and stack[-1] == "(":
+                stack.pop()
+            else:
+                stack.append(")")
+                break
 
-    if P1_cnt > P2_cnt:
-        print("Player 1")
-    elif P1_cnt < P2_cnt:
-        print("Player 2")
+    if len(stack) == 0:
+        print("yes")
     else:
-        print("TIE")
+        print("no")
