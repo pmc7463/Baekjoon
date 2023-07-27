5c7581255f4920bd0e6cecf0f8dd4a168d764f05
akscjfals@naver.com <akscjfals@naver.com>
2023-07-27 10:59:36 +0900

    [problem 4880] Next number

--- python.py
+++ python.py
@@ -1,28 +1,18 @@
 import sys
-from collections import deque
 
-repetition = int(sys.stdin.readline())
+while True:
+    a1, a2, a3 = map(int, sys.stdin.readline().split())
 
-balloon = []
+    if a1 == 0 and a2 == 0 and a3 == 0:
+        break
 
-for i in range(repetition):
-    balloon.append(i)
+    AP = a3 - a2
 
-order = list(map(int, sys.stdin.readline().split()))
+    if a2 - a1 == AP:
+        print(f"AP {a3 + AP}")
 
-zipped = zip(balloon, order)
-zip_dict = dict(zipped)
+    if a1 != 0 and a2 != 0 and a3 != 0:
+        GP = a3 / a2
 
-queue = deque(zip_dict.items())
-#print(queue)
-
-while queue:
-    balloon_num, shift_amount = queue.popleft()
-    balloon_num += 1
-
-    if shift_amount > 0:
-        queue.rotate(-(shift_amount-1))
-    else:
-        queue.rotate(-shift_amount)
-
-    print(balloon_num, end = " ")
+        if a2 / a1 == GP:
+            print(f"GP {int(a3 * GP)}")
