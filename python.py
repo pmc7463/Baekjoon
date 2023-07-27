86e58655ff4f6b8b905a54d129b7c4c90cb87ca6
akscjfals@naver.com <akscjfals@naver.com>
2023-07-27 16:05:33 +0900

    [problem 1237] 정ㅋ벅ㅋ

--- python.py
+++ python.py
@@ -1,18 +1,2 @@
-import sys
-
-while True:
-    a1, a2, a3 = map(int, sys.stdin.readline().split())
-
-    if a1 == 0 and a2 == 0 and a3 == 0:
-        break
-
-    AP = a3 - a2
-
-    if a2 - a1 == AP:
-        print(f"AP {a3 + AP}")
-
-    if a1 != 0 and a2 != 0 and a3 != 0:
-        GP = a3 / a2
-
-        if a2 / a1 == GP:
-            print(f"GP {int(a3 * GP)}")
+# 1237
+# 이 문제 만큼은 정답을 올릴 수 없다.
