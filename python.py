296ec5e730fdcff6953f354fa897d78cc4c6a9e9
akscjfals@naver.com <akscjfals@naver.com>
2023-07-28 12:29:12 +0900

    [problem 3078] A good friend

--- python.py
+++ python.py
@@ -1,2 +1,21 @@
-# 1237
-# 이 문제 만큼은 정답을 올릴 수 없다.
+import sys
+from collections import deque
+
+student, interval = map(int, sys.stdin.readline().split())
+
+cnt = 0
+name = [0] * student
+data = [0] * 21
+
+for i in range(student):
+    length = len(sys.stdin.readline().rstrip())
+    name[i] = length
+
+    if i > interval:
+        data[name[i - interval - 1]] -= 1
+
+    cnt += data[length]
+
+    data[length] += 1
+
+print(cnt)
