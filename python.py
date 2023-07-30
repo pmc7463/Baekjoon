961476ebfc4d55ebc008d65a0705757fd7170a27
akscjfals@naver.com <akscjfals@naver.com>
2023-07-30 22:40:15 +0900

    [problem 2863] Is this a fountain?

--- python.py
+++ python.py
@@ -1,47 +1,6 @@
-import sys
-from collections import deque
+A, B = map(int, input().split())
+C, D = map(int, input().split())
 
-def possible(sentence, arr):
-    j = 0
-    misscnt = 0
+arr = [A/C + B/D, C/D + A/B, D/B + C/A, B/A + D/C]
 
-    while sentence:
-        if arr[j] and sentence[0] == arr[j][0]:
-            arr[j].popleft()
-            sentence.popleft()
-            misscnt = 0
-        else:
-            if misscnt == N:
-                return False
-            misscnt += 1
-
-        j = (j + 1) % N
-
-    empty = 0
-
-    for a in range(N):
-        if len(arr[a]) == 0:
-            empty += 1
-
-    if not sentence and empty == N:
-        return True
-    else:
-        return False
-
-N = int(input())
-
-arr = list()
-
-for i in range(N):
-    arr.append(deque(map(str, input().split())))
-
-#print(arr)
-
-sentence = deque(map(str, input().split()))
-
-#print(sentence)
-
-if possible(sentence, arr):
-    print("Possible")
-else:
-    print("Impossible")
+print(arr.index(max(arr)))
