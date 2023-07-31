accfca08380f5a353b852466d354441574b694ac
akscjfals@naver.com <akscjfals@naver.com>
2023-07-31 22:58:02 +0900

    [problem 11586] 지영 공주님의 마법 거울

--- python.py
+++ python.py
@@ -1,6 +1,10 @@
-A, B = map(int, input().split())
-C, D = map(int, input().split())
+n = int(input())
+mirror = [input() for _ in range(n)]
+k = int(input())
 
-arr = [A/C + B/D, C/D + A/B, D/B + C/A, B/A + D/C]
-
-print(arr.index(max(arr)))
+if k == 1:    # 원본 출력
+    print(*mirror, sep='\n')
+elif k == 2:    # 좌우 반전
+    print(*[i[::-1] for i in mirror], sep='\n')
+else:    # 상하 반전
+    print(*mirror[::-1], sep='\n')
