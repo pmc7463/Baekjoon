9df6985d13b459164815265a80c9398d7c6b0ba2
akscjfals@naver.com <akscjfals@naver.com>
2023-08-05 18:59:13 +0900

    [problem 24883] 자동완성

--- python.py
+++ python.py
@@ -1,24 +1,5 @@
-import sys
-from collections import deque
-
-chair = int(sys.stdin.readline())
-
-human = list(input())
-
-#print(human)
-
-cnt = 0
-
-flag = 0
-
-for i in range(chair):
-    if human[i] == "L":
-        cnt += 1
-        flag = 1
-#print(cnt)
-
-
-if flag == 1:
-    print(chair - int(cnt / 2) + 1)
+check = input()
+if check == "N" or check == "n":
+    print("Naver D2")
 else:
-    print(chair - int(cnt / 2))
+    print("Naver Whale")
