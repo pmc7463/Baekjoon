01a025b80000a048ac7b611b00c52b217bdb18cf
changstudent <akscjfals@naver.com>
2023-08-07 23:00:30 +0900

    [problem 27866] 문자와 문자열

--- python.py
+++ python.py
@@ -1,11 +1,4 @@
-for _ in range(int(input())):
-    n = int(input())
-    if n < 3:
-        for i in range(n):
-            print('#'*n)
-        print()
-    else:
-        print('#'*n)
-        for i in range(n-2):
-            print('#' + 'J'*(n-2) + '#')
-        print('#'*n, '\n')
+Str = list(input())
+number = int(input())
+
+print(Str[number-1])
