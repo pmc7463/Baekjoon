02c9c45f60b57adb758fb7dbaeee05f30e5cacdc
akscjfals@naver.com <akscjfals@naver.com>
2023-08-09 17:23:07 +0900

    [problem 1715] 카드 정렬하기

--- python.py
+++ python.py
@@ -1,4 +1,31 @@
-Str = list(input())
-number = int(input())
+import heapq
+import sys
+from collections import deque
 
-print(Str[number-1])
+N = int(sys.stdin.readline())
+
+arr = []
+
+for i in range(N):
+    arr.append(0)
+
+for i in range(N):
+    arr[i] = int(sys.stdin.readline())
+
+arr = sorted(arr)
+
+#print(arr)
+
+result = 0
+
+if len(arr) == 1:
+    print(result)
+
+else:
+    for i in range(N-1):
+        pre = heapq.heappop(arr)
+        cur = heapq.heappop(arr)
+
+        result += pre + cur
+        heapq.heappush(arr, pre + cur)
+    print(result)
