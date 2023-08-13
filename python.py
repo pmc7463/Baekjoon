5a35ecac8d120907c296b395d0a66a2ed6ea42cd
akscjfals@naver.com <akscjfals@naver.com>
2023-08-13 20:23:54 +0900

    [problem 2997] 네 번째 수

--- python.py
+++ python.py
@@ -1,30 +1,12 @@
-import sys
-input = sys.stdin.readline
-
-repetition = int(input())
-
-arr = []
-
-for i in range(repetition):
-    a, b = map(int, input().split())
-    arr.append((a,b))
-
-arr.sort(key= lambda x:(x[1], x[0]))
-
-cnt = 0
+arr = list(map(int, input().split()))
 
 #print(arr)
-#print(arr[0][0])
-#print(arr[0][1])
 
-cnt = 0
+arr = sorted(arr)
 
-temp = 0
+#print(arr)
 
-for i in range(0,repetition):
-    if arr[i][0] >= temp:
-        if arr[i][0] <= arr[i][1]:
-            cnt += 1
-            temp = arr[i][1]
-            #print("temp : ",temp)
-print(cnt)
+if arr[1] + (arr[1] - arr[0]) == arr[2]:
+    print(arr[2] + (arr[1] - arr[0]))
+else:
+    print(arr[2] - (arr[1] - arr[0]))
