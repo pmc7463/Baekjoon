d690bac63daa2168507066b7f958f6e78371d039
akscjfals@naver.com <akscjfals@naver.com>
2023-08-15 17:10:25 +0900

    [problem 2979] 트럭 주차

--- python.py
+++ python.py
@@ -1,18 +1,29 @@
-def backtracking(N, M, arr, check):
-    if len(arr) == M:
-        print(" ".join(map(str, arr)))
-        return
+A, B, C = map(int, input().split())
 
-    for i in range(1,N+1):
-        if not check[i]:
-            check[i] = True
-            arr.append(i)
-            backtracking(N, M, arr, check)
-            arr.pop()
-            check[i] = False
+arr = [list(map(int, input().split())) for _ in range(3)]
 
-N, M = map(int, input().split())
+#print(arr) # 2차원 행렬
 
-check = [False] * (N+1)
+n = max(arr[0][1], arr[1][1], arr[2][1])
 
-backtracking(N, M, [], check)
+#print(n)  # 8
+
+board = [0] * (n-1)
+
+for truck in arr:
+    for i in range(truck[0]-1, truck[1]-1):
+        board[i] = board[i] + 1
+
+#print(board)    # 주차 대 수 카운터
+
+fee = 0
+
+for check in board:
+    if check == 1:
+        fee += check * A
+    elif check == 2:
+        fee += check * B
+    elif check == 3:
+        fee += check * C
+
+print(fee)
