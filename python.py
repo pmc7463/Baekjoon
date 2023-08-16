ed42dd746f06dc63dd5052dd8e1e06800a9818c4
akscjfals@naver.com <akscjfals@naver.com>
2023-08-16 15:44:01 +0900

    [problem 10799] 쇠막대기

--- python.py
+++ python.py
@@ -1,29 +1,22 @@
-A, B, C = map(int, input().split())
+stick = list(map(str, input()))
 
-arr = [list(map(int, input().split())) for _ in range(3)]
+# print(stick)
 
-#print(arr) # 2차원 행렬
+check = []
 
-n = max(arr[0][1], arr[1][1], arr[2][1])
+result = 0
 
-#print(n)  # 8
+for i in range(len(stick)):
+    if stick[i] == "(":
+        check.append("(")
 
-board = [0] * (n-1)
+    else:
+        if stick[i-1] == "(":
+            check.pop()
+            result += len(check)
 
-for truck in arr:
-    for i in range(truck[0]-1, truck[1]-1):
-        board[i] = board[i] + 1
+        else:
+            check.pop()
+            result += 1
 
-#print(board)    # 주차 대 수 카운터
-
-fee = 0
-
-for check in board:
-    if check == 1:
-        fee += check * A
-    elif check == 2:
-        fee += check * B
-    elif check == 3:
-        fee += check * C
-
-print(fee)
+print(result)
