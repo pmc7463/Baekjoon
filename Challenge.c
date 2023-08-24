0c65a85b4f31c009e747633250986fc3f2c16db4
akscjfals@naver.com <akscjfals@naver.com>
2023-08-24 20:28:24 +0900

    [problem 15964] 이상한 기호

--- Challenge.c
+++ Challenge.c
@@ -1,31 +1,18 @@
 #include <stdio.h>
 
+double func(double a, double b) 
+{
+    return (a + b) * (a - b);
+}
+
 int main(void)
 {   
-    char input[9];
-    int a, b, c;
-    int d, e, f;
-    int g, h ,i;
+    double A;
+    double B;
 
-    int time = 0;
+    scanf("%lf %lf", &A, &B);
 
-    scanf("%8s", input);
-    sscanf(input, "%d:%d:%d", &a, &b, &c);
+    printf("%0.lf\n", func(A, B));
 
-    scanf("%8s", input);
-    sscanf(input, "%d:%d:%d", &d, &e, &f);
-
-    time = d * 3600 + e * 60 + f - (a * 3600 + b * 60 + c);
-
-    if (time < 0)
-    {
-        time += 3600 * 24;
-    }
-
-    g = (int)time / 3600;
-    h = (int)(time % 3600) / 60;
-    i = time % 60;
-
-    printf("%02d:%02d:%02d", g, h, i);
     return 0;
 }
