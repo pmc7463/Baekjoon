10f4afee5b37faf49937056369edecfea0e3ebc0
akscjfals@naver.com <akscjfals@naver.com>
2023-08-25 18:15:36 +0900

    [problem 25191] 치킨 댄스를 추는 곰곰이를 본 임스

--- python.py
+++ python.py
@@ -1,13 +1,7 @@
-h1, m1, s1 = map(int, input().split(":"))
-h2, m2, s2 = map(int, input().split(":"))
-
-time = h2 * 3600 + m2 * 60 + s2 - (h1 * 3600 + m1 * 60 + s1);
-
-if time < 0:
-    time += 3600 * 24
-
-h = time // 3600
-m = (time % 3600) // 60
-s = time % 60
-
-print(f"{h:02d}:{m:02d}:{s:02d}")
+def sol(n,a,b):
+    return min(n,a//2+b)
+ 
+n=int(input())
+a,b = map(int, input().split())
+ 
+print(sol(n,a,b))
