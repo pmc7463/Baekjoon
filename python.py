a61c769be44a156f03c6c001721c7f0f26f95fc8
changstudent <akscjfals@naver.com>
2023-08-26 09:57:47 +0900

    [problem 6778] Which Alien?

--- python.py
+++ python.py
@@ -1,7 +1,11 @@
-def sol(n,a,b):
-    return min(n,a//2+b)
- 
-n=int(input())
-a,b = map(int, input().split())
- 
-print(sol(n,a,b))
+antenna = int(input())
+eye = int(input())
+
+if antenna >= 3 and eye <= 4:
+    print("TroyMartian")
+
+if antenna <= 6 and eye >= 2:
+    print("VladSaturnian")
+    
+if antenna <= 2 and eye <= 3:
+    print("GraemeMercurian")
