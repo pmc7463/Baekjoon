9b51268f1859677981de99fc89ebe2f19fd9bb22
akscjfals@naver.com <akscjfals@naver.com>
2023-08-30 23:57:55 +0900

    [problem 1389] 케빈 베이컨의 6단계 법칙

--- python.py
+++ python.py
@@ -1,10 +1,31 @@
-bread, meat = map(int, input().split())
+import sys
+from collections import deque
 
-bread //= 2
+def bfs(v):
+    queue = deque([v])
+    visited[v] = 1
 
-#print(bread)
+    while queue:
+        target = queue.popleft()
 
-if bread >= meat:
-    print(meat)
-else:
-    print(bread)
+        for i in graph[target]:
+            if not visited[i]:
+                visited[i] = visited[target] + 1
+                queue.append(i)
+
+n, m = map(int, sys.stdin.readline().split())
+
+graph = [[] for _ in range(n + 1)]
+for i in range(m):
+    a, b = map(int, sys.stdin.readline().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+res = []
+
+for i in range(1, n + 1):
+    visited = [0] * (n + 1)
+    bfs(i)
+    res.append(sum(visited))
+
+print(res.index(min(res)) + 1)
