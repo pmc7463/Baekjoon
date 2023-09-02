02f16d5b24177556be8d48421942bcb1cfa13152
akscjfals@naver.com <akscjfals@naver.com>
2023-09-02 17:28:07 +0900

    [problem 1764] 듣보잡

--- python.py
+++ python.py
@@ -1,13 +1,46 @@
-N, P = map(int, input().split())
+listening, view = map(int, input().split())
 
-arr = []
-N_temp = N
+L = [0] * listening
+V = [0] * view
 
-while True:
-    N_temp = int(N_temp * N) % P
-    
-    if N_temp in arr:
-        print(len(arr) - arr.index(N_temp))
-        break
+for i in range(listening):
+    L[i] = input()
 
-    arr.append(N_temp)
+for i in range(view):
+    V[i] = input()
+
+#print("************")
+
+#print(L)
+#print(V)
+"""
+cnt = 0
+
+save = [0] * (listening + view)
+
+for i in range(len(L)):
+    for j in range(len(V)):
+        if L[i] == V[j]:
+            cnt += 1
+            save[i] = V[j]
+
+print(cnt)
+#print(save)
+
+filtered_list = sorted([item for item in save if isinstance(item, str)])
+
+#print(filtered_list)
+
+for i in filtered_list:
+    print(i)
+"""
+#marge = set(L + V)
+
+#print(marge)
+
+result = sorted(list(set(L) & set(V)))
+print(len(result))
+#print(result)
+
+for i in result:
+    print(i)
