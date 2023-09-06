fd50ff524339b72da037afb07db09e5dcc56367c
akscjfals@naver.com <akscjfals@naver.com>
2023-09-06 22:48:15 +0900

    [problem 2563] 색종이

--- python.py
+++ python.py
@@ -1,9 +1,16 @@
-import sys
-input = sys.stdin.readline
+arr = [[0 for _ in range(101)] for _ in range(101)]
+number = int(input())
 
-N = int(input())
+for _ in range(number):
+    x, y = list(map(int, input().split()))
 
-if N % 2 == 0:
-    print("CY")
-else:
-    print("SK")
+    for row in range(x, x+10):
+        for col in range(y, y+10):
+            arr[row][col] = 1
+
+result = 0
+
+for i in arr:
+    result += i.count(1)
+
+print(result)
