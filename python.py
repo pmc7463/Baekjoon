10efef5f14ab458f4bf1a9da86ead21ee72f1920
akscjfals@naver.com <akscjfals@naver.com>
2023-09-09 23:50:52 +0900

    [problem 24267] 알고리즘 수업 - 알고리즘의 수행시간6

--- python.py
+++ python.py
@@ -1,17 +1,3 @@
-a, b = map(int, input().split())
-
-arr = []
-cnt = 0
-temp = 0
-
-if a > b:
-    temp = a
-    a = b
-    b = temp
-
-for i in range(a+1, b):
-    cnt += 1
-    arr.append(str(i))
-
-print(cnt)
-print(' '.join(arr))
+n = int(input())
+print(((n-2)*(n-1)*(2*n-3)+3*(n-1)*(n-2))//12)
+print(3)
