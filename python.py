7179faad793a100ff75039f82cbf714793d0deac
akscjfals@naver.com <akscjfals@naver.com>
2023-09-10 21:30:26 +0900

    [problem 9933] 민균이의 비밀번호

--- python.py
+++ python.py
@@ -1,3 +1,13 @@
 n = int(input())
-print(((n-2)*(n-1)*(2*n-3)+3*(n-1)*(n-2))//12)
-print(3)
+
+word = []
+
+for _ in range(n):
+    word.append(input())
+
+#print(word)
+
+for i in range(n):
+    for j in range(i, n):
+        if len(word[i]) == len(word[j]) and word[i][::-1] == word[j]:
+            print(len(word[i]), word[i][int(len(word[i])/2)])
