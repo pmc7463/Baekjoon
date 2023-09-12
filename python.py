d173e00168574da3fc6125a6eeb42e82941cb2db
akscjfals@naver.com <akscjfals@naver.com>
2023-09-12 21:42:36 +0900

    [problem 11024] 더하기 4

--- python.py
+++ python.py
@@ -1,24 +1,9 @@
-A, B = input().split()
-N, M = len(A), len(B)
+test = int(input())
 
-arr = [['.'] * N for _ in range(M)]
+temp = 0
 
-for i in range(N):
-    flag = False
-    for j in range(M):
-        if A[i] == B[j]:
-            row = j
-            col = i
-            flag = True
-            break
-            
-    if flag == True:
-        break
-for i in range(N):
-    arr[row][i] = A[i]
+for _ in range(test):
+    arr = list(map(int, input().split()))
+    temp = sum(arr)
 
-for i in range(M):
-    arr[i][col] = B[i]
-
-for i in range(M):
-    print(''.join(arr[i]))
+    print(temp)
