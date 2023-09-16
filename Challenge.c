9ab4ff4467daf94cf28a6ec231190499eb281faf
akscjfals@naver.com <akscjfals@naver.com>
2023-09-16 23:09:28 +0900

    [problem 2667] 단지번호붙이기

---
+++ Challenge.c
@@ -0,0 +1,59 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int n = 0;
+int count = 0;
+int sum = 0;
+
+int graph[26][26] = { 0 };
+int apart[26 * 26] = { 0 };
+
+int dx[4] = {-1, 1, 0, 0};
+int dy[4] = { 0, 0, -1, 1 };
+
+int dfs(int x, int y) {
+	if (x < 0 || y < 0 || x >= n || y >= n) {
+		return 0;
+	}
+	if (graph[x][y] == 1) {
+		graph[x][y] = 0;
+		count++;
+
+		for (int i = 0; i < 4; i++) {
+			dfs(x + dx[i], y + dy[i]);
+		}
+		return 1;
+	}
+	return 0;
+}
+
+int main(void) {
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			scanf("%1d", graph[i] + j);
+		}
+	}
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (dfs(i, j) == 1) {
+				apart[count]++;
+				count = 0;
+				sum++;
+			}
+		}
+	}
+	printf("%d\n", sum);
+
+	for (int i = 0; i < 26 * 26; i++) {
+		if (apart[i] != 0) {
+			int k = apart[i];
+			for (int j = 0; j < k; j++) {
+				printf("%d\n", i);
+			}
+		}
+	}
+	return 0;
+}
