8a23cf3c87c852e4baf9e304f0791ddcae03ce8d
akscjfals@naver.com <akscjfals@naver.com>
2023-09-26 20:10:57 +0900

    [problem 25206] 너의 평점은

--- Challenge.c
+++ Challenge.c
@@ -1,24 +1,44 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    int chicken = 0;
-    int a[3];
-    int sum = 0;
+    char subject[51];
+    double num;
+    char grade[3];
 
-    scanf("%d", &chicken);
-    scanf("%d %d %d", &a[0], &a[1], &a[2]);
+    double sum = 0.0;
+    double temp;
+    double gs = 0.0;
 
-    for (int i = 0; i < 3; i++) {
-        if (a[i] <= chicken) {
-            sum += a[i];
+    for (int i = 0; i < 20; i++) {
+        scanf("%s %lf %s", subject, &num, grade);
+    
+        if (grade[0] == 80)
+            continue;
+
+        sum += num;
+
+        if (grade[0] == 70)
+            continue;
+
+        if (grade[0] == 65)
+            temp = 4;
+        else if (grade[0] == 66)
+            temp = 3;
+        else if (grade[0] == 67)
+            temp = 2;
+        else
+            temp = 1;
+
+        if (grade[1] == 43) {
+            temp += 0.5;
         }
-        else {
-            sum += chicken;
-        }
+
+        gs += num * temp;
     }
-
-    printf("%d\n", sum);
+    
+    printf("%lf", gs/sum);
 
     return 0;
 }
