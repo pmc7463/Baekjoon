4eab5485343e8d63d4209b2a29419c786693cfb1
Min Fe 97 <77577896+pmc7463@users.noreply.github.com>
2023-10-09 19:48:26 +0900

    [problem 14592]2017 아주대학교 프로그래밍 경시대회 (Small)

--- python.py
+++ python.py
@@ -1,7 +1,7 @@
-t = input()
-cnt = 0
-for _ in range(int(input())):
-    s = input()
-    if t in s*2:
-        cnt += 1
-print(cnt)
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+lst = [list(map(int, input().split())) for _ in range(N)]
+sorted_lst = sorted(lst, key=lambda x:(-x[0], x[1], x[2]))
+print(lst.index(sorted_lst[0])+1)
