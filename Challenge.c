65d3d78a349331bd58fb1babe6c01bbf5f859e76
akscjfals@naver.com <akscjfals@naver.com>
2023-10-18 17:27:57 +0900

    [problem 15667] 2018 연세대학교 프로그래밍 경진대회

--- Challenge.c
+++ Challenge.c
@@ -2,36 +2,19 @@
 #include <stdio.h>
 
 int main() {
-	int sum[200];
-	int arr[200][3];
-	int test;
-	int check;
+	int f;
+	int check = 0;
 
-	scanf("%d", &test);
+	scanf("%d", &f);
+	f -= 1;
 
-	for (int i = 0; i < test; i++) {
-		scanf("%d %d %d", &arr[i][0], &arr[i][1], &arr[i][2]);
+	while (1) {
+		if (check * check + check == f)
+			break;
+		check++;
 	}
-
-	for (int n = 0; n < 3; n++) {
-		for (int j = 0; j < test; j++) {
-			check = 0;
-			for (int p = j + 1; p < test; p++) {
-				if (arr[j][n] == arr[p][n]) {
-					check = 1;
-					arr[p][n] = 0;
-				}
-			}
-			if (check) {
-				arr[j][n] = 0;
-			}
-		}
-	}
-
-	for (int k = 0; k < test; k++) {
-		sum[k] = arr[k][0] + arr[k][1] + arr[k][2];
-		printf("%d\n", sum[k]);
-	}
+	
+	printf("%d", check);
 
 	return 0;
 }
