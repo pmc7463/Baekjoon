8c6d39b1238eaa562d8773f8aae0f14f153827c6
akscjfals@naver.com <akscjfals@naver.com>
2023-10-19 21:12:02 +0900

    [problem 13597] Tri-du

--- python.py
+++ python.py
@@ -1,16 +1,3 @@
-N, K = map(int, input().split())
+a, b = map(int, input().split())
 
-arr = list(map(int, input().split(',')))
-
-temp = []
-
-for i in range(K):
-    for j in range(len(arr) - 1):
-        temp.append(arr[j+1] - arr[j])
-
-    arr = temp
-    temp = []
-
-arr = list(map(str, arr))
-
-print(','.join(arr))
+print(max(a,b))
