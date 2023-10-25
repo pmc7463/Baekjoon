5a46250a5bcbb2252fa4662e49988212a97bed84
akscjfals@naver.com <akscjfals@naver.com>
2023-10-25 15:58:31 +0900

    [problem 1356] 유진수

--- python.py
+++ python.py
@@ -1,3 +1,29 @@
-a, b = map(int, input().split())
+from functools import reduce
 
-print(max(a,b))
+def multiply(arr):
+    return reduce(lambda x, y: x * y, arr)
+
+num = int(input())
+
+arr = []
+
+flag = 1
+
+while num != 0:
+    temp = num % 10
+    arr.append(temp)
+    num = int(num / 10)
+
+length = len(arr)
+
+for i in range(length):
+    for j in range(1, length):
+        if multiply(arr[i:j]) == multiply(arr[i+1:]):
+            flag = 1
+
+    if flag == 1:
+        print("Yes")
+        break
+    else:
+        print("No")
+        break
