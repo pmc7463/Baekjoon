832e309d04335028c909b1a3197c81ef964b0c32
Min Fe 97 <77577896+pmc7463@users.noreply.github.com>
2023-10-28 23:33:48 +0900

    [poblem 15947] 아기 석환 뚜루루 뚜루

--- python.py
+++ python.py
@@ -1,29 +1,10 @@
-from functools import reduce
-
-def multiply(arr):
-    return reduce(lambda x, y: x * y, arr)
-
-num = int(input())
-
-arr = []
-
-flag = 1
-
-while num != 0:
-    temp = num % 10
-    arr.append(temp)
-    num = int(num / 10)
-
-length = len(arr)
-
-for i in range(length):
-    for j in range(1, length):
-        if multiply(arr[i:j]) == multiply(arr[i+1:]):
-            flag = 1
-
-    if flag == 1:
-        print("Yes")
-        break
-    else:
-        print("No")
-        break
+li = ["baby", "sukhwan", "tururu", "turu", "very", "cute", "tururu", 
+      "turu", "in", "bed", "tururu", "turu", "baby", "sukhwan"]
+N = int(input())-1
+k = N//14
+if N%14 in [3, 7, 11]:
+    print(f"tu+ru*{k+1}" if k >= 4 else "turu"+"ru"*k)    
+elif N%14 in [2, 6, 10]:
+    print(f"tu+ru*{k+2}" if k >= 3 else "tururu"+"ru"*k)    
+else:
+    print(li[N%14])
