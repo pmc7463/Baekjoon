21ee7befad73f47135e9499ac3af3aa392d8a20a
Min Fe 97 <77577896+pmc7463@users.noreply.github.com>
2023-10-30 23:04:25 +0900

    [problem 1233] 주사위

--- Challenge.c
+++ Challenge.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    int N;
-    int cnt = 0;
+    int arr[81] = {0,};
+    int a, b, c;
+    int temp, flag;
+    int max = 0;
 
-    scanf("%d", &N);
+    scanf("%d %d %d", &a, &b, &c);
 
-    for (int i = 1; i <= 1000; i++) {
-        for (int j = 1; j <= 1000; j++) {
-            if (pow(i,2) + N == pow(j,2)) {
-                cnt++;
-                break;           
-            }
-            else if (pow(i,2) + N == pow(j,2)) {
-                break;
+    for (int i = 1; i <= a; i++) {
+        for (int j = 1; j <= b; j++) {
+            for (int k = 1; k <= c; k++) {
+                temp = i + j+ k;
+                arr[temp] += 1;            
             }
         }
-    } 
-    printf("%d\n", cnt);
+    }
+
+    for (int p = 1; p <= 80; p++) {
+        if (arr[p] > max) {
+            max = arr[p];
+            flag = p;
+        }
+    }
+    
+    printf("%d\n", flag);
 
     return 0;
 }
