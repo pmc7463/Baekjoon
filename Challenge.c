e44dfb06d453ec2f58d34a93ea7137a67b16f224
akscjfals@naver.com <akscjfals@naver.com>
2023-11-02 21:40:28 +0900

    [problem 2702] 초6 수학

--- Challenge.c
+++ Challenge.c
@@ -1,44 +1,28 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
+
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
 
 int main() {
-    int* arr;
-    int size = 1000000;
-    int num;
-    int i = 0;
-    int left = 0, mid, right = 0;
+    int a, b;
+    int temp;
+    int test;
 
-    arr = (int*)malloc(size * sizeof(int));
+    scanf("%d", &test);
 
-    scanf("%d", &num);
+    for (int i = 0; i < test; i++) {
 
-    while (num > 0) {
-        arr[i] = num % 10;
-        num /= 10;
-        i++;
+        scanf("%d %d", &a, &b);
+
+        temp = gcd(a, b);
+
+        printf("%d %d\n", (a * b) / temp, temp);
     }
 
-    mid = (i + 1) / 2;
-
-    for (int j = 0; j < mid; j++) {
-        left += arr[j];
-    }
-
-    //printf("\n");
-
-    for (int k = mid; k < mid*2; k++) {
-        right += arr[k];
-    }
-
-    if (left == right) {
-        printf("LUCKY");
-    }
-    else {
-        printf("READY");
-    }
-
-    free(arr);
-    
     return 0;
 }
