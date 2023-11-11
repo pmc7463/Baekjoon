aec85c6ef37085804ee97ed46a446e5dceffbf65
akscjfals@naver.com <akscjfals@naver.com>
2023-11-11 17:13:47 +0900

    test

--- Challenge.c
+++ Challenge.c
@@ -1,28 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    int n, m, i, j, temp;
-    int beg, mid, end;
-    int *arr;
+int main() {
+    int a, b, c;
 
-    scanf("%d %d", &n, &m);
-    arr = malloc(sizeof(int) * n);
-    for (i = 0; i < n; i++)
-        arr[i] = i + 1;
-    for (i = 0; i < m; i++)
-    {
-        scanf("%d %d %d", &beg, &end, &mid);
-        for (; beg - 1 < mid - 1; mid--, end--)
-        {
-            temp = arr[mid - 2];
-            for (j = mid - 1; j <= end - 1; j++)
-                arr[j - 1] = arr[j];
-            arr[end - 1] = temp;
-        }
-    }
-    for (int j = 0; j < n; j++)
-        printf("%d ", arr[j]);
-    free(arr);
-}
+    do {
+        scanf("%d %d", &a, &b);
+
+        c = a * 3 - a - b;
+        if (a != 0 && b != 0) 
+            printf("%d\n", c);
+    } while (a != 0 && b != 0);
+
+    return 0;
+} 
