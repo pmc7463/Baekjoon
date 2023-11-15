bcc8c1bdbc4b8558ab020ff2ecd0de3c67018f72
akscjfals@naver.com <akscjfals@naver.com>
2023-11-15 22:36:43 +0900

    [problem 16394] 홍익대학교

--- Challenge.c
+++ Challenge.c
@@ -1,22 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <math.h>
 
 int main() {
-    float row, col, x, y;
-    int cnt = 0;
+    int year;
 
-    int a, b;
+    scanf("%d", &year);
 
-    scanf("%f %f %f %f", &col , &row, &x, &y);
-
-    a = ceil(col / (x + 1));
-    b = ceil(row / (y + 1));
-
-    //printf("%d\n", a);
-    //printf("%d\n", b);
-    printf("%d\n", a * b);
-    
-
+    printf("%d\n", year - 1946);
     return 0;
 }
