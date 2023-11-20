c78a147e47596730d1241905ecf0bf98bcd1f4c3
akscjfals@naver.com <akscjfals@naver.com>
2023-11-20 21:30:10 +0900

    [problem 1252] 이진수 덧셈

--- python.py
+++ python.py
@@ -1,7 +1,13 @@
-from datetime import datetime
+grid = [[0 for _ in range(1001)] for _ in range(1001)]
+num_of_papers = int(input())
 
-now = datetime.now()
-
-print(now.year)
-print(now.month)
-print(now.day)
+for p in range(1, num_of_papers+1):
+    p_x, p_y, width, height = map(int, input().split())
+    for y in range(p_y, p_y+height):
+        grid[y][p_x:(p_x+width)] = [p]*width
+            
+for p in range(1, num_of_papers+1):
+    result = 0
+    for cnt in range(1001):
+        result += grid[cnt].count(p)
+    print(result)
