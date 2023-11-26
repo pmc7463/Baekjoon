05125beb0aeff9e516dbe31ca2ebf7173c11d5b2
Min Fe 97 <77577896+pmc7463@users.noreply.github.com>
2023-11-27 01:08:06 +0900

    [problem 20254] Site Score

--- Challenge.c
+++ Challenge.c
@@ -1,18 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
 
 int main() {
-    long long int sum = 0;
-    char a[10001], b[10001];
-
-    scanf("%s %s", a, b);
-
-    for (int i = 0; i < strlen(a); i++)
-        for (int j = 0; j < strlen(b); j++)
-            sum += (a[i] - '0') * (b[j] - '0');
-
-    printf("%lld", sum);
-
-    return 0;
+    int UR, TR, UO, TO; 
+    
+    scanf("%d %d %d %d", &UR, &TR, &UO, &TO);
+    
+    printf("%d", 56*UR + 24*TR + 14*UO + 6*TO);
+    
+return 0;
 }
